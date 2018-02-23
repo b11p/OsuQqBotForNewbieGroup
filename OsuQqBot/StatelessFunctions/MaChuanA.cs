f3ba03e19980d6d186a46e9eef1a97ed093cd9bd
bleatingsheep <bleatingsheep@live.cn>
2018-02-23 18:38:30 +0800

    增加发与白菜相同的stat图的功能

---
+++ OsuQqBot/StatelessFunctions/MaChuanA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OsuQqBot.Api;
+using OsuQqBot.LocalData;
+using OsuQqBot.QqBot;
+
+namespace OsuQqBot.StatelessFunctions
+{
+    class MaChuanA : IStatelessFunction
+    {
+        public bool ProcessMessage(EndPoint endPoint, MessageSource messageSource, string message)
+        {
+            message = message.Trim();
+            if (message == "妈船？" || message == "妈船?")
+            {
+                long? uid = Database.Instance.GetUidFromQq(messageSource.FromQq);
+                if (!uid.HasValue) return true;
+                string url = MotherShipApi.GetStatUrl(uid.Value);
+                var api = OsuQqBot.QqApi;
+                string content = api.OnlineImage(url);
+                api.SendMessageAsync(endPoint, content);
+            }
+            return false;
+        }
+    }
+}
