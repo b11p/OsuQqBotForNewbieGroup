27c0d4fea0402ecba12dc64deca9ae9e3fb1bc83
bleatingsheep <bleatingsheep@live.cn>
2018-04-03 15:36:41 +0800

    修复 Konachan 部分图片无法发送，移除 debug 信息。

--- OsuQqBot/StatelessFunctions/Konachan.cs
+++ OsuQqBot/StatelessFunctions/Konachan.cs
@@ -17,12 +17,11 @@ namespace OsuQqBot.StatelessFunctions
         private async void ProcessAsync(EndPoint endPoint, string message)
         {
             var k = new Moebooru.Api(website);
-            var (result, info) = await k.PopularRecentDebugAsync();
+            var result = await k.PopularRecentAsync();
             var qq = OsuQqBot.QqApi;
-            qq.SendMessageAsync(endPoint, qq.BeforeSend(info));
             if (!result.Any()) return;
             var post = result.First();
-            qq.SendMessageAsync(endPoint, qq.OnlineImage(post.FileUrl));
+            qq.SendMessageAsync(endPoint, qq.OnlineImage(post.JpegUrl));
         }
     }
 }
