3cf5cad092896adbfedc86a325778218805237cc
bleatingsheep <bleatingsheep@live.cn>
2018-04-26 15:02:39 +0800

    IRegularly

---
+++ OsuQqBot/AttributedFunctions/IRegularly.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsuQqBot.AttributedFunctions
+{
+    interface IRegularly
+    {
+        TimeSpan? OnUtc { get; }
+        TimeSpan? Every { get; }
+    }
+}
