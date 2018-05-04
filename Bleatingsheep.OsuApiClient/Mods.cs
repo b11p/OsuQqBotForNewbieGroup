c543631126759ade6fd3c634aa374bf09ae53b3a
bleatingsheep <bleatingsheep@live.cn>
2018-05-05 01:43:25 +0800

    增加从字符串转换 Mod，以及把 Mod 转换成习惯上的字符串表示功能。

--- Bleatingsheep.OsuApiClient/Mods.cs
+++ Bleatingsheep.OsuApiClient/Mods.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bleatingsheep.OsuMixedApi
 {
@@ -36,5 +38,101 @@ namespace Bleatingsheep.OsuMixedApi
         Key1 = 67108864,
         Key3 = 134217728,
         Key2 = 268435456
+    }
+
+    public static class ModsExtensions
+    {
+        private static readonly IReadOnlyDictionary<string, Mods> pairs = new Dictionary<string, Mods>
+        {
+            //{ "NONE", Mods.None },
+            { "NF", Mods.NoFail },
+            { "EZ", Mods.Easy },
+            { "HD", Mods.Hidden },
+            { "HR", Mods.HardRock },
+            { "SD", Mods.SuddenDeath },
+            { "DT", Mods.DoubleTime },
+            // Relax
+            { "HT", Mods.HalfTime },
+            { "NC", Mods.Nightcore },
+            { "FL", Mods.Flashlight },
+            { "PF", Mods.Perfect },
+        };
+
+        private static readonly IReadOnlyDictionary<Mods, Mods> require = new Dictionary<Mods, Mods>
+        {
+            { Mods.Nightcore, Mods.DoubleTime },
+            { Mods.Perfect, Mods.SuddenDeath },
+        };
+
+        private static IEnumerable<string> Split(string modString)
+        {
+            if (modString.Length % 2 != 0) throw new ArgumentException("String argument is invalid.", nameof(modString));
+            for (int i = 0; i < modString.Length; i += 2)
+            {
+                yield return modString.Substring(i, 2);
+            }
+        }
+
+        public static Mods Parse(string modString)
+        {
+            if (modString == null)
+            {
+                throw new ArgumentNullException(nameof(modString));
+            }
+
+            modString = modString.ToUpperInvariant();
+
+            if (modString == string.Empty || modString == "NONE")
+            {
+                return Mods.None;
+            }
+
+            Mods result = Mods.None;
+
+            foreach (string mod in Split(modString))
+            {
+                if (!pairs.TryGetValue(mod, out Mods current)) throw new ArgumentException("String argument is invalid.", nameof(modString));
+
+                current |= require.GetValueOrDefault(current);
+
+                if ((current & result) != 0) throw new ArgumentException("Repeated mods were found.");
+
+                result |= current;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get the display string of mods.
+        /// </summary>
+        /// <param name="mods"></param>
+        /// <returns></returns>
+        public static string Display(this Mods mods)
+        {
+            foreach (Mods mod in require.Where(r => (r.Key & mods) != 0).Select(r => r.Value))
+            {
+                mods &= ~mod;
+            }
+
+            bool needSp = false;
+            string result = string.Empty;
+
+            foreach (var (mod, description) in pairs.Where(p => mods.HasFlag(p.Value)).Select(p => (p.Value, p.Key)).ToArray())
+            {
+                needSp = true;
+                result += description;
+                mods &= ~mod;
+            }
+
+            if (mods != 0)
+            {
+                if (needSp) result += ", ";
+
+                result += mods.ToString();
+            }
+
+            return result;
+        }
     }
 }
