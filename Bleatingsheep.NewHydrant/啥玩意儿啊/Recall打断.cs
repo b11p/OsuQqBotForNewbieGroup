ce8b29c98552e69d5a04685bec0f016409ce2bcf
bleatingsheep <bleatingsheep@live.cn>
2018-07-18 22:34:26 +0800

    打断也撤回一下吧。

---
+++ Bleatingsheep.NewHydrant/啥玩意儿啊/Recall打断.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Bleatingsheep.NewHydrant.Attributions;
+using Sisters.WudiLib;
+using Sisters.WudiLib.Posts;
+
+namespace Bleatingsheep.NewHydrant.啥玩意儿啊
+{
+    [Function("recall_old_hydrant")]
+    internal class Recall打断 : IMessageMonitor
+    {
+        public async Task OnMessageAsync(Sisters.WudiLib.Posts.Message message, HttpApiClient api)
+        {
+            if (message.UserId == 122866607 && message is GroupMessage g && g.GroupId == 641236878)
+            {
+                await api.RecallMessageAsync(message.MessageId);
+            }
+        }
+    }
+}
