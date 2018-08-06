aece25b7e8023f03509183e136e1dc5288467285
bleatingsheep <bleatingsheep@live.cn>
2018-08-06 20:59:00 +0800

    更新新 rank 图提醒逻辑。

--- Bleatingsheep.NewHydrant/Osu/NewlyRankedBeatmapNotify.cs
+++ Bleatingsheep.NewHydrant/Osu/NewlyRankedBeatmapNotify.cs
@@ -18,7 +18,9 @@ namespace Bleatingsheep.NewHydrant.Osu
         private static IEnumerable<long> s_groups = new List<long>
         {
             72318078,
-            672076603,
+            672076603, // HTTP 机器人。
+            514661057, // 后花园。
+            308419061, // Steam。
         };
 
         private readonly object _thisLock = new object();
@@ -31,35 +33,40 @@ namespace Bleatingsheep.NewHydrant.Osu
             var result = await BloodcatApi.Client.SearchRankedByKeywordAsync();
             lock (_thisLock)
             {
-                var logger = executingInfo.Logger;
-                logger.LogInBackground($"取到 {result.Count()} 个");
-                logger.LogInBackground($"日期晚于最新：{result.Where(s => s.ApprovedDateOffset > _noBeatmapAfter).Count()} 个");
+                //var logger = executingInfo.Logger;
+                //logger.LogInBackground($"取到 {result.Count()} 个");
+                //logger.LogInBackground($"日期晚于最新：{result.Where(s => s.ApprovedDateOffset > _noBeatmapAfter).Count()} 个");
                 if (!(_oldSets is null))
                 {
-                    logger.LogInBackground($"提取前 {_oldSets.Count()} 个");
+                    //logger.LogInBackground($"提取前 {_oldSets.Count()} 个");
                     newSets = result.Take(_oldSets.Count()).Concat(result.Where(s => s.ApprovedDateOffset > _noBeatmapAfter))
                         .Where(s => !_oldSets.Contains(s.Id))
                         .Distinct()
                         .ToList();// 避免延迟求值
-                    logger.LogInBackground($"发送 {newSets.Count()} 个");
+                    //logger.LogInBackground($"发送 {newSets.Count()} 个");
                 }
                 _oldSets = result.Select(s => s.Id).ToHashSet();
                 if (_noBeatmapAfter < result.Max(s => s.ApprovedDateOffset))
                 {
                     _noBeatmapAfter = result.Max(s => s.ApprovedDateOffset);
-                    logger.LogInBackground($"日期更新为 {_noBeatmapAfter}");
+                    //logger.LogInBackground($"日期更新为 {_noBeatmapAfter}");
                 }
             }
             var osu = executingInfo.OsuApi;
             if (newSets != null)
             {
                 var qq = executingInfo.Qq;
-                foreach (var set in newSets)
+                const int limit = 5;
+                var messages = newSets.Take(limit).Select(s => BloodcatUtilities.GetMusicMessage(osu, s)).ToList();
+                foreach (long group in s_groups)
                 {
-                    Message message = BloodcatUtilities.GetMusicMessage(osu, set);
-                    foreach (long group in s_groups)
+                    foreach (Message message in messages)
                     {
                         await qq.SendGroupMessageAsync(group, message);
+                    }
+                    if (newSets.Count() > limit)
+                    {
+                        await qq.SendGroupMessageAsync(group, $"共有 {newSets.Count()} 张新图，只显示前 {limit} 张。");
                     }
                 }
             }
