c0384817eca3ff16651e3c692d8c3bdde1efa1ed
bleatingsheep <bleatingsheep@live.cn>
2018-10-06 11:35:09 +0800

    禁用V2的功能。

--- Bleatingsheep.NewHydrant/Osu/ApiV2.cs
+++ Bleatingsheep.NewHydrant/Osu/ApiV2.cs
@@ -1,29 +1,29 @@
-﻿using System.IO;
-using System.Reflection;
-using System.Threading.Tasks;
-using Bleatingsheep.NewHydrant.Attributions;
-using Bleatingsheep.NewHydrant.Core;
-using Bleatingsheep.Osu.ApiV2;
+﻿//using System.IO;
+//using System.Reflection;
+//using System.Threading.Tasks;
+//using Bleatingsheep.NewHydrant.Attributions;
+//using Bleatingsheep.NewHydrant.Core;
+//using Bleatingsheep.Osu.ApiV2;
 
-namespace Bleatingsheep.NewHydrant.Osu
-{
-    [Function("api2_provider")]
-    internal class ApiV2 : IInitializable
-    {
-        public static OsuApiV2Client Client { get; private set; }
+//namespace Bleatingsheep.NewHydrant.Osu
+//{
+//    [Function("api2_provider")]
+//    internal class ApiV2 : IInitializable
+//    {
+//        public static OsuApiV2Client Client { get; private set; }
 
-        public string Name { get; } = "apiv2";
+//        public string Name { get; } = "apiv2";
 
-        public async Task<bool> InitializeAsync(ExecutingInfo executingInfo)
-        {
-            var authPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "..", "config", "authv2.txt");
-            var lines = await File.ReadAllLinesAsync(authPath);
-            if (lines.Length != 2)
-                return false;
-            string username = lines[0];
-            string password = lines[1];
-            Client = new OsuApiV2Client(username, password);
-            return true;
-        }
-    }
-}
+//        public async Task<bool> InitializeAsync(ExecutingInfo executingInfo)
+//        {
+//            var authPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "..", "config", "authv2.txt");
+//            var lines = await File.ReadAllLinesAsync(authPath);
+//            if (lines.Length != 2)
+//                return false;
+//            string username = lines[0];
+//            string password = lines[1];
+//            Client = new OsuApiV2Client(username, password);
+//            return true;
+//        }
+//    }
+//}
