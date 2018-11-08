b93446ff98d2730279a6b0945b67b620616edea3
bleatingsheep <bleatingsheep@live.cn>
2018-11-09 01:36:10 +0800

    增加获取 IP 地址的 Controller，并且正确处理反代。

---
+++ NewHydrantApi/Controllers/MyIPController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewHydrantApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyIPController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public MyIPController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("-----direct-----");
+
+            var connection = _httpContextAccessor.HttpContext.Connection;
+            sb.AppendLine(new IPEndPoint(connection.RemoteIpAddress, connection.RemotePort).ToString());
+
+            var request = _httpContextAccessor.HttpContext.Request;
+            sb.AppendLine(request.Scheme);
+            sb.AppendLine(request.Host.ToString());
+            sb.AppendLine(request.Protocol);
+            sb.AppendLine(request.IsHttps.ToString());
+            sb.AppendLine(request.Method);
+
+            sb.AppendLine("-----forwarded-----");
+
+            var headers = request.Headers;
+            sb.AppendLine(headers["X-Forwarded-For"]);
+            sb.AppendLine(headers["X-Forwarded-Proto"]);
+            sb.AppendLine(headers["X-Forwarded-Host"]);
+
+            sb.AppendLine("-----original-----");
+            sb.AppendLine(headers["X-Original-For"]);
+            sb.AppendLine(headers["X-Original-Proto"]);
+            sb.AppendLine(headers["X-Original-Host"]);
+
+            return sb.ToString();
+        }
+    }
+}
--- NewHydrantApi/Startup.cs
+++ NewHydrantApi/Startup.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -25,11 +29,23 @@ namespace NewHydrantApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // proxy
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.AllowedHosts.Add("api.bleatingsheep.org");
+                options.KnownProxies.Add(IPAddress.Parse("2002:bf65:af98:c::cc:be"));
+                options.ForwardedHeaders = ForwardedHeaders.All;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // proxy
+            app.UseForwardedHeaders();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
