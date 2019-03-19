0f51da657837b238288c940ecf676b50e00e8689
bleatingsheep <bleatingsheep@live.cn>
2019-03-20 02:08:19 +0800

    知乎日报 Chrome 类独立。

---
+++ Bleatingsheep.NewHydrant.Bot/啥玩意儿啊/Chrome.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using PuppeteerSharp;
+
+namespace Bleatingsheep.NewHydrant.啥玩意儿啊
+{
+    public static class Chrome
+    {
+        private static readonly object s_launchLock = new object();
+
+        private static Browser s_browser;
+
+        public static Func<Browser> GetBrowser { get; private set; } = () =>
+        {
+            lock (s_launchLock)
+            {
+                if (s_browser == null)
+                {
+                    s_browser = Puppeteer.LaunchAsync(new LaunchOptions
+                    {
+                        Headless = true,
+                        ExecutablePath = @"/opt/google/chrome/chrome",
+                        DefaultViewport = new ViewPortOptions
+                        {
+                            DeviceScaleFactor = 1,
+                            Width = 360,
+                            Height = 640,
+                        },
+                        Args = new[] { "--no-sandbox" },
+                    }).GetAwaiter().GetResult();
+                }
+                if (s_browser != null)
+                    GetBrowser = () => s_browser;
+                return s_browser;
+            }
+        };
+
+        public static async Task<Page> OpenNewPageAsync()
+            => await GetBrowser().NewPageAsync();
+    }
+}
--- Bleatingsheep.NewHydrant.Bot/啥玩意儿啊/知乎日报.cs
+++ Bleatingsheep.NewHydrant.Bot/啥玩意儿啊/知乎日报.cs
@@ -14,35 +14,6 @@ namespace Bleatingsheep.NewHydrant.啥玩意儿啊
     [Function("zhihu_daily")]
     public class 知乎日报 : IMessageCommand
     {
-        private static readonly object s_launchLock = new object();
-
-        private static Browser s_browser;
-
-        private static Func<Browser> GetBrowser { get; set; } = () =>
-        {
-            lock (s_launchLock)
-            {
-                if (s_browser == null)
-                {
-                    s_browser = Puppeteer.LaunchAsync(new LaunchOptions
-                    {
-                        Headless = true,
-                        ExecutablePath = @"/opt/google/chrome/chrome",
-                        DefaultViewport = new ViewPortOptions
-                        {
-                            DeviceScaleFactor = 3,
-                            Width = 360,
-                            Height = 350,
-                        },
-                        Args = new[] { "--no-sandbox" },
-                    }).GetAwaiter().GetResult();
-                }
-                if (s_browser != null)
-                    GetBrowser = () => s_browser;
-                return s_browser;
-            }
-        };
-
         public async Task ProcessAsync(MessageContext context, HttpApiClient api)
         {
             string url = "https://rss.bleatingsheep.org/zhihu/daily";
@@ -52,14 +23,14 @@ namespace Bleatingsheep.NewHydrant.啥玩意儿啊
             var item = feed.Items.LastOrDefault();
             if (item != default)
             {
-                using (var page = await GetBrowser().NewPageAsync())
+                using (var page = await Chrome.OpenNewPageAsync())
                 {
                     await page.SetContentAsync(item.Summary.Text);
                     await page.SetViewportAsync(new ViewPortOptions
                     {
-                        DeviceScaleFactor = 3,
+                        DeviceScaleFactor = 1,
                         Width = 360,
-                        Height = 350,
+                        Height = 640,
                     });
                     var data = await page.ScreenshotDataAsync(new ScreenshotOptions
                     {
