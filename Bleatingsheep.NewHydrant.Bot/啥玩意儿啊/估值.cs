1798ca6b7123690572465ff20fc1b41ad6cc79d5
bleatingsheep <bleatingsheep@live.cn>
2019-06-08 12:36:14 +0800

    增加指数估值展示功能，来源且慢。

---
+++ Bleatingsheep.NewHydrant.Bot/啥玩意儿啊/估值.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bleatingsheep.NewHydrant.Attributions;
+using Bleatingsheep.NewHydrant.Core;
+using PuppeteerSharp;
+using Sisters.WudiLib;
+using Message = Sisters.WudiLib.SendingMessage;
+using MessageContext = Sisters.WudiLib.Posts.Message;
+
+namespace Bleatingsheep.NewHydrant.啥玩意儿啊
+{
+    [Function("指数估值")]
+    class 估值 : Service, IMessageCommand
+    {
+        public async Task ProcessAsync(MessageContext context, HttpApiClient api)
+        {
+            using (var page = await Chrome.OpenNewPageAsync())
+            {
+                await page.SetViewportAsync(new ViewPortOptions
+                {
+                    DeviceScaleFactor = 2,
+                    Width = 618,
+                    Height = 2000,
+                });
+                await page.GoToAsync("https://qieman.com/idx-eval");
+
+                //// Get update time.
+                //const string updateDateSelector = "#app > div > div.ant-layout-container > div > div > div.ant-col-xs-24.ant-col-lg-15 > div > div > div.sc-jWBwVP.dtKXxN > div > span.qm-header-note > div > p";
+                //var updateDateElement = await page.WaitForSelectorAsync(updateDateSelector);
+                //if (updateDateElement != null)
+                //{
+                //    var property = await updateDateElement.GetPropertyAsync("textContent");
+                //    if (property != null)
+                //    {
+                //        var textContent = (await property.JsonValueAsync()).ToString();
+                //        await api.SendMessageAsync(context.Endpoint, textContent);
+                //    }
+                //    else
+                //    {
+                //        Logger.Debug($"{nameof(property)} is null.");
+                //    }
+                //}
+                //else
+                //{
+                //    Logger.Debug($"{nameof(updateDateElement)} is null.");
+                //}
+
+                //await Task.Delay(TimeSpan.FromSeconds(3));
+                //var xp = await page.QuerySelectorAsync("#app > div > div.ant-layout-container > div > div > div.ant-col-xs-24.ant-col-lg-15 > div > div > div.flex-table__67b31");
+                //if (xp != null)
+                //    await api.SendMessageAsync(context.Endpoint, Message.ByteArrayImage(await xp.ScreenshotDataAsync(new ScreenshotOptions
+                //    {
+
+                //    })));
+
+
+                const string detailSelector = "#app > div > div.ant-layout-container > div > div > div.ant-col-xs-24.ant-col-lg-15 > div > div";
+                ElementHandle detailElement = await page.WaitForSelectorAsync(detailSelector);
+                var data = await detailElement
+                    .ScreenshotDataAsync(new ScreenshotOptions
+                    {
+                        //FullPage = true,
+                    });
+
+                await api.SendMessageAsync(context.Endpoint, Message.ByteArrayImage(data));
+            }
+        }
+
+        public bool ShouldResponse(MessageContext context)
+        {
+            return context.Content.TryGetPlainText(out string text)
+                && string.Equals(text, "估值", StringComparison.Ordinal);
+        }
+    }
+}
