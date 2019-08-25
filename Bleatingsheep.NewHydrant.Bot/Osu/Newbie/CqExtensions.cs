e8495b4aad2f979d07926fa453b98289761b913b
bleatingsheep <bleatingsheep@live.cn>
2019-08-26 00:46:06 +0900

    增强加群请求分析功能。

---
+++ Bleatingsheep.NewHydrant.Bot/Osu/Newbie/CqExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Sisters.WudiLib;
+
+namespace Bleatingsheep.NewHydrant.Osu.Newbie
+{
+
+    public partial class LevelInfo
+    {
+        [JsonProperty("level")]
+        public int Level { get; set; }
+
+        [JsonProperty("level_speed")]
+        public double LevelSpeed { get; set; }
+
+        [JsonProperty("nickname")]
+        public string Nickname { get; set; }
+
+        [JsonProperty("user_id")]
+        public long UserId { get; set; }
+
+        [JsonProperty("vip_growth_speed")]
+        public int VipGrowthSpeed { get; set; }
+
+        [JsonProperty("vip_growth_total")]
+        public int VipGrowthTotal { get; set; }
+
+        [JsonProperty("vip_level")]
+        public string VipLevel { get; set; }
+    }
+
+    internal static class CqHttpApiExtensions
+    {
+        public static async Task<LevelInfo> GetLevelInfo(this HttpApiClient api, long qq)
+        {
+            var levelInfo = await api.CallAsync<LevelInfo>("_get_vip_info", new { user_id = qq });
+            return levelInfo;
+        }
+    }
+}
--- Bleatingsheep.NewHydrant.Bot/Osu/Newbie/NotifyOnJoinRequest.cs
+++ Bleatingsheep.NewHydrant.Bot/Osu/Newbie/NotifyOnJoinRequest.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Bleatingsheep.NewHydrant.Attributions;
@@ -32,11 +37,65 @@ namespace Bleatingsheep.NewHydrant.Osu.Newbie
             await HintBinding(api, endpoint, userId);
         }
 
+        private async Task ParseInfoAsync(
+            HttpApiClient api,
+            Endpoint sendBackEndpoint,
+            long userId,
+            int? osuId = null,
+            OsuMixedApi.UserInfo userInfo = default,
+            string comment = null)
+        {
+            var hints = new List<string>();
+            var levelInfo = await api.GetLevelInfo(userId);
+            if (levelInfo != null)
+            {
+                hints.Add($"QQ 等级为 {levelInfo.Level}");
+            }
+            if (!string.IsNullOrEmpty(comment))
+            {
+                var userNames = OsuHelper.DiscoverUsernames(comment).Where(n => !string.Equals(n, "osu", StringComparison.OrdinalIgnoreCase));
+                if (userInfo != null && userNames.Any(n => string.Equals(userInfo.Name, n, StringComparison.OrdinalIgnoreCase)))
+                {// 绑定一致
+
+                }
+                else if (osuId == null)
+                {// 忽略已绑定的情况，因为可能绑定不一致或者查询失败。
+                    foreach (var name in userNames)
+                    {
+                        var (success, info) = await OsuApi.GetUserInfoAsync(name, Bleatingsheep.Osu.Mode.Standard);
+                        // 我想用 8.0 新语法
+                        hints.Add($"{info?.Name ?? name}: " +
+                            $"{(success ? info == null ? "不存在此用户。" : $"PP: {info.Performance}, PC: {info.PlayCount}, TTH: {info.TotalHits}" : "查询失败。")}");
+
+                        // 提供绑定并放行的捷径。
+                        if (info?.Performance < 2500)
+                        {
+                            var ms = new MemoryStream();
+                            using (var bw = new BinaryWriter(ms, Encoding.UTF8, true))
+                            {
+                                bw.Write(userId);
+                                bw.Write(info.Id);
+                            }
+                            ms.Write(MD5.Create().ComputeHash(ms.ToArray()));
+                            var bytes = ms.ToArray();
+                            var base64 = Convert.ToBase64String(bytes);
+                            await api.SendMessageAsync(sendBackEndpoint, $"（占位）绑定为 {info.Name} 并放行：#{base64}#");
+                        }
+                    }
+                }
+            }
+            if (hints.Any())
+            {
+                await api.SendMessageAsync(sendBackEndpoint, string.Join("\r\n", hints));
+            }
+        }
+
         private async ValueTask<double?> HintBinding(HttpApiClient api, Endpoint endpoint, long userId, string comment = null)
         {
             var (success, osuId) = await DataProvider.GetBindingIdAsync(userId);
             string response = string.Empty;
             double? performance = default;
+            OsuMixedApi.UserInfo user = null;
             if (!success)
             {
                 response = "查询失败";
@@ -44,14 +103,25 @@ namespace Bleatingsheep.NewHydrant.Osu.Newbie
 
             else if (osuId == null)
             {
-                response = "这个人没绑定。其余信息的查询不做了没有了。";
+                response = "这个人没绑定。";
             }
             else
             {
-                var (osuApiGood, user) = await OsuApi.GetUserInfoAsync(osuId.Value, OsuMixedApi.Mode.Standard);
+                bool osuApiGood;
+                (osuApiGood, user) = await OsuApi.GetUserInfoAsync(osuId.Value, OsuMixedApi.Mode.Standard);
                 performance = user?.Performance;
 
                 response = $"这个人绑定的 uid 是 {osuId}，用户名是 {(osuApiGood ? user?.Name ?? "被办了" : "查询失败")}\r\n（正在施工）";
+            }
+
+            // 提供额外信息
+            try
+            {
+                await ParseInfoAsync(api, endpoint, userId, osuId, user, comment);
+            }
+            catch (Exception e)
+            {
+                await api.SendMessageAsync(endpoint, e.ToString());
             }
 
             // 保留 comment
