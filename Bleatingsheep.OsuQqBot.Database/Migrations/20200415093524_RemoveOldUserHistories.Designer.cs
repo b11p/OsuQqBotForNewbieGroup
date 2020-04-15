0acd49942ce385357caa0f13c592b3c4cd182c9e
bleatingsheep <bleatingsheep@live.cn>
2020-04-15 18:37:14 +0900

    Add migration.

--- Bleatingsheep.OsuQqBot.Database/Migrations/20200415093524_RemoveOldUserHistories.Designer.cs
+++ Bleatingsheep.OsuQqBot.Database/Migrations/20200415093524_RemoveOldUserHistories.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Bleatingsheep.OsuQqBot.Database.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bleatingsheep.OsuQqBot.Database.Migrations
 {
     [DbContext(typeof(NewbieContext))]
-    partial class NewbieContextModelSnapshot : ModelSnapshot
+    [Migration("20200415093524_RemoveOldUserHistories")]
+    partial class RemoveOldUserHistories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
