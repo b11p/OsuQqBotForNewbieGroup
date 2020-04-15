182ba275b195d13980c026ec56a797a5185e57c3
bleatingsheep <bleatingsheep@live.cn>
2020-04-15 16:56:46 +0900

    Remove charts.

--- Bleatingsheep.OsuQqBot.Database/Models/NewbieContext.cs
+++ Bleatingsheep.OsuQqBot.Database/Models/NewbieContext.cs
@@ -7,11 +7,6 @@ namespace Bleatingsheep.OsuQqBot.Database.Models
 {
     public class NewbieContext : DbContext
     {
-        public DbSet<Chart> Charts { get; private set; }
-        public DbSet<ChartBeatmap> ChartBeatmaps { get; private set; }
-        public DbSet<ChartValidGroup> ChartValidGroups { get; private set; }
-        public DbSet<ChartAdministrator> ChartAdministrators { get; private set; }
-        public DbSet<ChartTry> ChartTries { get; private set; }
         public DbSet<Beatmap> CachedBeatmaps { get; private set; }
         public DbSet<OperationHistory> Histories { get; private set; }
         public DbSet<BindingInfo> Bindings { get; private set; }
@@ -31,18 +26,6 @@ namespace Bleatingsheep.OsuQqBot.Database.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Ignore<Osu.ApiClient.UserEvent>();
-
-            modelBuilder.Entity<ChartAdministrator>()
-                .HasKey(a => new { a.ChartId, a.Administrator });
-
-            modelBuilder.Entity<ChartBeatmap>()
-                .HasKey(m => new { m.ChartId, m.BeatmapId, m.Mode });
-
-            modelBuilder.Entity<ChartValidGroup>()
-                .HasKey(g => new { g.ChartId, g.GroupId });
-
-            modelBuilder.Entity<ChartTry>()
-                .HasKey(c => new { c.ChartId, c.BeatmapId, c.Mode, c.Date, c.UserId });
 
             modelBuilder.Entity<GroupMemberInfo>()
                 .HasKey(mg => new { mg.GroupName, mg.UserId });
