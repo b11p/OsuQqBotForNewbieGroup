e16edae54f131a56c8125d5ccdba5aef6cdb2771
bleatingsheep <bleatingsheep@live.cn>
2020-05-03 02:12:28 +0900

    Add migration.

--- Bleatingsheep.OsuQqBot.Database/Migrations/20200502170954_SnapshotIndex.Designer.cs
+++ Bleatingsheep.OsuQqBot.Database/Migrations/20200502170954_SnapshotIndex.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Bleatingsheep.OsuQqBot.Database.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bleatingsheep.OsuQqBot.Database.Migrations
 {
     [DbContext(typeof(NewbieContext))]
-    partial class NewbieContextModelSnapshot : ModelSnapshot
+    [Migration("20200502170954_SnapshotIndex")]
+    partial class SnapshotIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Bleatingsheep.OsuQqBot.Database/Migrations/20200502170954_SnapshotIndex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bleatingsheep.OsuQqBot.Database.Migrations
+{
+    public partial class SnapshotIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Version",
+                table: "UpdateSchedules",
+                rowVersion: true,
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp(6)",
+                oldNullable: true)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSnapshots_UserId_Mode_Date",
+                table: "UserSnapshots",
+                columns: new[] { "UserId", "Mode", "Date" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserSnapshots_UserId_Mode_Date",
+                table: "UserSnapshots");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Version",
+                table: "UpdateSchedules",
+                type: "timestamp(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldRowVersion: true,
+                oldNullable: true)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+        }
+    }
+}
