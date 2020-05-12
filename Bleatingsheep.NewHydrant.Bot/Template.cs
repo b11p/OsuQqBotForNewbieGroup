e4bf21052fd2e7d5a759a433bf29206e29d34233
bleatingsheep <bleatingsheep@live.cn>
2020-05-12 16:18:00 +0900

    Add template file.

---
+++ Bleatingsheep.NewHydrant.Bot/Template.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Bleatingsheep.NewHydrant.Attributions;
+using Message = Sisters.WudiLib.SendingMessage;
+using MessageContext = Sisters.WudiLib.Posts.Message;
+
+namespace $rootnamespace$
+{
+    [Function("$itemname$")]
+    public class $safeitemname$
+    {
+    }
+}
