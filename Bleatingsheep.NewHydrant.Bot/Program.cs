b11p/OsuQqBotForNewbieGroup

commit 3b39b2346c81987f6c15d44f91615f1e632bfd12

Fix chart not logging.

Bleatingsheep.NewHydrant.Bot/Program.cs
@@ -59,19 +59,19 @@ namespace Bleatingsheep.NewHydrant
                 apiPostListener.ForwardTo = "http://oldbot:8876";
                 apiPostListener.StartListen();
 
-                // 添加必要的事件处理。
-                apiPostListener.FriendRequestEvent += ApiPostListener.ApproveAllFriendRequests;
-                apiPostListener.GroupRequestEvent += (api, e) => e.UserId == configure.SuperAdmin ? new GroupRequestResponse { Approve = true } : null;
-                apiPostListener.GroupInviteEvent += (api, e) => new GroupRequestResponse { Approve = true };
-                //apiPostListener.GroupAddedEvent += (api, e) => api.SetGroupCard(e.GroupId, e.SelfId, _configure.Name).Wait();
-                apiPostListener.GroupRequestEvent += new NotifyOnJoinRequest().Monitor;
-                apiPostListener.GroupBanEvent += new 啥玩意儿啊.难受_宁愿不能说话的人是我().EvilDalou;
-
                 // 配置 osu
                 OsuFunction.SetApiKey(configure.ApiKey);
 
                 var hydrant = new Hydrant(httpApiClient, apiPostListener, Assembly.GetExecutingAssembly(), typeof(Hydrant).Assembly)
                     .AddLogger(LogManager.LogFactory);
+
+                // 添加必要的事件处理。
+                apiPostListener.FriendRequestEvent += ApiPostListener.ApproveAllFriendRequests;
+                apiPostListener.GroupRequestEvent += (api, e) => e.UserId == configure.SuperAdmin ? new GroupRequestResponse { Approve = true } : null;
+                apiPostListener.GroupInviteEvent += (api, e) => new GroupRequestResponse { Approve = true };
+                //apiPostListener.GroupAddedEvent += (api, e) => api.SetGroupCard(e.GroupId, e.SelfId, _configure.Name).Wait();
+                apiPostListener.GroupRequestEvent += hydrant.CreateServiceInstance<NotifyOnJoinRequest>().Monitor;
+                apiPostListener.GroupBanEvent += new 啥玩意儿啊.难受_宁愿不能说话的人是我().EvilDalou;
 
                 // 设置异常处理。
                 hydrant.ExceptionCaught_Command += Hydrant_ExceptionCaught_Command;

Bleatingsheep.NewHydrant/Bleatingsheep.NewHydrant.csproj
@@ -9,7 +9,7 @@
 
   <ItemGroup>
     <PackageReference Include="Autofac" Version="5.2.0" />
-    <PackageReference Include="NLog" Version="4.6.7" />
+    <PackageReference Include="NLog" Version="4.7.2" />
   </ItemGroup>
 
   <ItemGroup>

OsuQqBot/OsuQqBot.csproj
@@ -9,7 +9,7 @@
     <PackageReference Include="Bleatingsheep.Osu.Helper" Version="1.0.0" />
     <PackageReference Include="IndexRange" Version="1.0.0" />
     <PackageReference Include="Newtonsoft.Json" Version="12.0.3" />
-    <PackageReference Include="NLog" Version="4.6.7" />
+    <PackageReference Include="NLog" Version="4.7.2" />
     <PackageReference Include="Sisters.WudiLib" Version="0.0.5" />
   </ItemGroup>
 

OsuQqBotHttp/OsuQqBotHttp.csproj
@@ -8,7 +8,7 @@
 
   <ItemGroup>
     <PackageReference Include="Newtonsoft.Json" Version="12.0.3" />
-    <PackageReference Include="NLog" Version="4.6.7" />
+    <PackageReference Include="NLog" Version="4.7.2" />
     <PackageReference Include="Sisters.WudiLib.WebSocket" Version="0.0.4" />
   </ItemGroup>
 

commit 4b350e43cacf99c54afbae287a0c298fd800f66f

Fix fatal bug introduced in 3b39b234.

Bleatingsheep.NewHydrant.Bot/Program.cs
@@ -65,6 +65,11 @@ namespace Bleatingsheep.NewHydrant
                 var hydrant = new Hydrant(httpApiClient, apiPostListener, Assembly.GetExecutingAssembly(), typeof(Hydrant).Assembly)
                     .AddLogger(LogManager.LogFactory);
 
+                // 设置异常处理。
+                hydrant.ExceptionCaught_Command += Hydrant_ExceptionCaught_Command;
+
+                hydrant.Init();
+
                 // 添加必要的事件处理。
                 apiPostListener.FriendRequestEvent += ApiPostListener.ApproveAllFriendRequests;
                 apiPostListener.GroupRequestEvent += (api, e) => e.UserId == configure.SuperAdmin ? new GroupRequestResponse { Approve = true } : null;
@@ -73,12 +78,10 @@ namespace Bleatingsheep.NewHydrant
                 apiPostListener.GroupRequestEvent += hydrant.CreateServiceInstance<NotifyOnJoinRequest>().Monitor;
                 apiPostListener.GroupBanEvent += new 啥玩意儿啊.难受_宁愿不能说话的人是我().EvilDalou;
 
-                // 设置异常处理。
-                hydrant.ExceptionCaught_Command += Hydrant_ExceptionCaught_Command;
-
-                hydrant.Init();
-
                 Console.WriteLine("init complete.");
+
+                hydrant.Run();
+                Console.WriteLine("Running...");
 
                 Task.Delay(-1).Wait();
             }

Bleatingsheep.NewHydrant/Core/Hydrant.cs
@@ -121,6 +121,7 @@ namespace Bleatingsheep.NewHydrant.Core
         private readonly Task _plan;
         #endregion
 
+        #region init and run
         public void Init() => Init<IHydrantStartup>(default!);
 
         public void Init<T>(T startup) where T : IHydrantStartup
@@ -133,60 +134,8 @@ namespace Bleatingsheep.NewHydrant.Core
             }
         }
 
-        internal static string GetServiceName(object? hit)
+        public void Run()
         {
-            var type = hit?.GetType() ?? typeof(Hydrant);
-            var attr = type.GetCustomAttribute<FunctionAttribute>();
-            return attr?.Name ?? type.Name;
-        }
-
-        private void LogException(string name, string? message, Exception e)
-        {
-            var logger = _logFactory?.GetLogger(name) ?? LogManager.CreateNullLogger();
-            logger.Warn(e, message);
-        }
-
-        #region Create Service Instance
-        public T CreateServiceInstance<T>() where T : notnull, Service
-        {
-            using var scope = _container.BeginLifetimeScope();
-            return CreateServiceInstance<T>(typeof(T), scope);
-        }
-
-        private object CreateServiceInstance(Type type, IComponentContext componentContext)
-        {
-            var result = type.CreateInstance(componentContext);
-            ConfigureDefaultService(result);
-            return result;
-        }
-
-        private T CreateServiceInstance<T>(Type type, IComponentContext componentContext) where T : notnull
-        {
-            var result = type.CreateInstance<T>(componentContext);
-            ConfigureDefaultService(result);
-            return result;
-        }
-
-        private void ConfigureDefaultService(object result)
-        {
-            if (result is Service s)
-            {
-                s.LogFactory = _logFactory;
-            }
-        }
-        #endregion
-
-        private void Init(IEnumerable<Assembly> assemblies, ContainerBuilder builder)
-        {
-            var types = assemblies.SelectMany(a => a.GetTypes()
-                .Where(t => t.GetCustomAttributes<FunctionAttribute>().Any()))
-                .ToList();
-
-            types.ForEach(t => builder.RegisterType(t));
-            _container = builder.Build();
-
-            types.ForEach(InitType);
-
             _listener.MessageEvent += async (api, message) =>
             {
                 await _messageMonitorList.ForEachAsync(async m =>
@@ -265,6 +214,63 @@ namespace Bleatingsheep.NewHydrant.Core
                 _plan.Start();
             }
         }
+        #endregion
+
+        internal static string GetServiceName(object? hit)
+        {
+            var type = hit?.GetType() ?? typeof(Hydrant);
+            var attr = type.GetCustomAttribute<FunctionAttribute>();
+            return attr?.Name ?? type.Name;
+        }
+
+        private void LogException(string name, string? message, Exception e)
+        {
+            var logger = _logFactory?.GetLogger(name) ?? LogManager.CreateNullLogger();
+            logger.Warn(e, message);
+        }
+
+        #region Create Service Instance
+        public T CreateServiceInstance<T>() where T : notnull, Service
+        {
+            using var scope = _container.BeginLifetimeScope();
+            return CreateServiceInstance<T>(typeof(T), scope);
+        }
+
+        private object CreateServiceInstance(Type type, IComponentContext componentContext)
+        {
+            var result = type.CreateInstance(componentContext);
+            ConfigureDefaultService(result);
+            return result;
+        }
+
+        private T CreateServiceInstance<T>(Type type, IComponentContext componentContext) where T : notnull
+        {
+            var result = type.CreateInstance<T>(componentContext);
+            ConfigureDefaultService(result);
+            return result;
+        }
+
+        private void ConfigureDefaultService(object result)
+        {
+            if (result is Service s)
+            {
+                s.LogFactory = _logFactory;
+            }
+        }
+        #endregion
+
+        #region init private
+        private void Init(IEnumerable<Assembly> assemblies, ContainerBuilder builder)
+        {
+            var types = assemblies.SelectMany(a => a.GetTypes()
+                .Where(t => t.GetCustomAttributes<FunctionAttribute>().Any()))
+                .ToList();
+
+            types.ForEach(t => builder.RegisterType(t));
+            _container = builder.Build();
+
+            types.ForEach(InitType);
+        }
 
         internal void InitType(Type t)
         {
@@ -311,6 +317,7 @@ namespace Bleatingsheep.NewHydrant.Core
             if (task.OnUtc is TimeSpan onUtc)
                 _regularTasks.Add(new ScheduleInfo(ScheduleType.Daily, onUtc, task));
         }
+        #endregion
 
         #region ExceptionEvent
 