ddabaf2cc6721a664db95a5ca901e4abd7cc5df4
bleatingsheep <bleatingsheep@live.cn>
2020-07-13 23:17:02 +0900

    Move 称金币 to 啥玩意儿啊.

---
+++ Bleatingsheep.NewHydrant.Bot/啥玩意儿啊/称金币.ExecuteInfo.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Bleatingsheep.NewHydrant.啥玩意儿啊
+{
+#nullable enable
+    public partial class 称金币
+    {
+        private class ExecuteInfo
+        {
+            public const int Max = 9;
+
+            private List<(int heavy, int light)> Known { get; } = new List<(int, int)>();
+            private readonly List<string> _knownForHints = new List<string>();
+            public ReadOnlyCollection<string> KnownForHints => _knownForHints.ToList().AsReadOnly();
+            public int WeighingCount => Known.Count;
+
+            private IReadOnlyCollection<IList<int>> Potentials { get; set; } = Enumerable.Empty<IList<int>>().ToList();
+
+            public ExecuteInfo()
+            {
+                AddCompare(1, 2, GetRandomBool(), false);
+                InitPotantials();
+            }
+
+            private static bool GetRandomBool() => DateTimeOffset.Now.ToUnixTimeMilliseconds() % 2 == 0;
+
+            private void AddCompare(int left, int right, bool leftIsHeavier, bool duplicated)
+            {
+                if (!duplicated)
+                    Known.Add(leftIsHeavier ? (left, right) : (right, left));
+                _knownForHints.Add($"{left} {(leftIsHeavier ? "重过" : "轻于")} {right}" + (duplicated ? " (重复)" : ""));
+            }
+
+            private void InitPotantials()
+            {
+                var current = Enumerable.Range(1, Max).Select(i => new[] { i } as IList<int>).ToList();
+                for (int i = 1; i < Max; i++)
+                {
+                    current =
+                        (from k in current
+                         from j in Enumerable.Range(1, Max)
+                         where !k.Contains(j) && !k.Any(present => Known.Contains((present, j)))
+                         select (k.Append(j).ToList()))
+                         .ToList<IList<int>>();
+                }
+                Potentials = current;
+            }
+
+            private (int, int) GetPotentialsCount(int left, int right)
+            {
+                Span<int> potentials = stackalloc int[2] { 0, 0 };
+
+                var scenarios = Potentials;
+                foreach (var item in scenarios)
+                {
+                    //var weightIndex = item.Select((w, i) => (index: i, weight: w));
+                    if (item.IndexOf(left) > item.IndexOf(right))
+                    {
+                        // left is heavier
+                        potentials[0]++;
+                    }
+                    else if (item.IndexOf(left) < item.IndexOf(right))
+                    {
+                        // right is heavier
+                        potentials[1]++;
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+
+                return (potentials[0], potentials[1]);
+            }
+
+            public IList<string> GetKnown(out bool isClear)
+            {
+                lock (Known)
+                {
+                    isClear = Potentials.Count <= 1;
+                    return KnownForHints;
+                }
+            }
+
+            public IList<string> Weigh(int left, int right, out bool isClear, out bool isKnown, out IList<int>? result)
+            {
+                lock (Known)
+                {
+                    var (hl, hr) = GetPotentialsCount(left, right);
+                    if (hl == 0 || hr == 0)
+                    {
+                        isKnown = true;
+                        isClear = false;
+                        result = null;
+                        AddCompare(left, right, hr == 0, true);
+                        return KnownForHints;
+                    }
+                    bool leftHeavy = (hl - hr) switch
+                    {
+                        var i when i > 0 => true,
+                        var i when i < 0 => false,
+                        0 => GetRandomBool(),
+                        _ => GetRandomBool(),
+                    };
+                    AddCompare(left, right, leftHeavy, false);
+                    //Known.Add(leftHeavy ? (left, right) : (right, left));
+                    isKnown = false;
+                    Potentials = Potentials.Where(potential =>
+                    {
+                        Span<int> weights = stackalloc int[Max];
+                        for (int i = 0; i < Max; i++)
+                        {
+                            weights[potential[i] - 1] = i + 1;
+                        }
+                        foreach (var (heavy, light) in Known)
+                        {
+                            if (weights[heavy - 1] < weights[light - 1])
+                                return false;
+                        }
+                        return true;
+                    }).ToList();
+                    isClear = Potentials.Count <= 1;
+                    result = isClear switch
+                    {
+                        true => Potentials.FirstOrDefault()?.ToList(),
+                        false => null,
+                    };
+                    return KnownForHints;
+                }
+            }
+        }
+    }
+#nullable restore
+}
---
+++ Bleatingsheep.NewHydrant.Bot/啥玩意儿啊/称金币.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Bleatingsheep.NewHydrant.Attributions;
+using Microsoft.Extensions.Caching.Memory;
+using Sisters.WudiLib;
+using Message = Sisters.WudiLib.SendingMessage;
+using MessageContext = Sisters.WudiLib.Posts.Message;
+
+namespace Bleatingsheep.NewHydrant.啥玩意儿啊
+{
+#nullable enable
+    [Component("称金币")]
+    public partial class 称金币 : IMessageCommand
+    {
+
+        private static readonly IMemoryCache s_cache = new MemoryCache(new MemoryCacheOptions());
+
+        private const int Max = ExecuteInfo.Max;
+
+        private bool _isInit;
+
+        private int _left;
+        private int _right;
+
+        private string GetInfo(IList<string> cp)
+        {
+            var l = cp.Last();
+            var sb = new StringBuilder("最近一次结果是：").Append(l).Append("\r\n");
+            sb.AppendJoin("\r\n", cp);
+            return sb.ToString();
+        }
+
+        private async Task Init(MessageContext context, HttpApiClient api)
+        {
+            var sb = new StringBuilder();
+            if (s_cache.TryGetValue(context.UserId, out ExecuteInfo exec))
+            {
+                sb.AppendLine("当前游戏正在进行。");
+            }
+            else
+            {
+                exec = s_cache.Set(context.UserId, new ExecuteInfo(), new MemoryCacheEntryOptions
+                {
+                    SlidingExpiration = TimeSpan.FromMinutes(10)
+                });
+            }
+            sb.Append("你有").Append(Max).Append("枚重量各不相同的金币，编号为1-").Append(Max).Append(@"，和一个天平。你可以用天平比较两枚金币的重量。
+你的目标是：称最少的次数（9枚金币19次，不知道是否有解），将这些金币按从轻到重排序
+你可以使用指令“称xy”来比较两枚金币的重量，其中x、y都是金币编号。
+例如：称12
+你无须提交排序结果，消防栓会自动检测你能否排出来。
+------
+");
+            string info = GetInfo(exec.GetKnown(out _));
+            sb.Append(info);
+            string message = sb.ToString();
+            await api.SendMessageAsync(context.Endpoint, message).ConfigureAwait(false);
+        }
+
+        private async Task Weigh(MessageContext context, HttpApiClient api)
+        {
+            if (!s_cache.TryGetValue<ExecuteInfo>(context.UserId, out var exec))
+            {
+                await api.SendMessageAsync(context.Endpoint, "你未开始游戏！").ConfigureAwait(false);
+                return;
+            }
+
+            if (_left <= 0 || _left > Max || _right <= 0 || _right > Max)
+            {
+                await api.SendMessageAsync(context.Endpoint, "称量金币错误。").ConfigureAwait(false);
+                return;
+            }
+
+            if (_left == _right)
+            {
+                await api.SendMessageAsync(context.Endpoint, "你怎么把一枚金币放到天平两边？").ConfigureAwait(false);
+                return;
+            }
+
+            var sb = new StringBuilder();
+            var cp = exec.Weigh(_left, _right, out bool isClear, out bool isKnown, out IList<int>? result);
+            if (isKnown)
+            {
+                sb.AppendLine("你已经知道哪边更重了，不是吗？");
+            }
+
+            sb.Append(GetInfo(cp));
+            if (isClear)
+            {
+                s_cache.Remove(context.UserId);
+                sb.Append("\r\n恭喜排序完成，共称了").Append(exec.WeighingCount).Append("次。");
+                if (exec.WeighingCount <= 19)
+                {
+                    await api.SendMessageAsync(context.Endpoint, "恭喜达到最优次数！").ConfigureAwait(false);
+                    await api.SendPrivateMessageAsync(962549599, $"{context.UserId} 达到最优19次！").ConfigureAwait(false);
+                    await api.SendPrivateMessageAsync(962549599, GetInfo(cp)).ConfigureAwait(false);
+
+                }
+                if (result != null)
+                {
+                    sb.AppendLine().AppendLine("从轻到重分别为：").AppendJoin(", ", result);
+                }
+            }
+            string message = sb.ToString();
+            await api.SendMessageAsync(context.Endpoint, message).ConfigureAwait(false);
+        }
+
+        public Task ProcessAsync(MessageContext context, HttpApiClient api)
+        {
+            return _isInit ? Init(context, api) : Weigh(context, api);
+        }
+
+        public bool ShouldResponse(MessageContext context)
+        {
+            if (context.Content.TryGetPlainText(out string text))
+            {
+                if (string.Equals(text, "称金币", StringComparison.Ordinal))
+                {
+                    _isInit = true;
+                    return true;
+                }
+                else if (Regex.IsMatch(text.Trim(), @"^称\d\d$", RegexOptions.Compiled))
+                {
+                    _left = int.Parse(text.Trim().Substring(1, 1));
+                    _right = int.Parse(text.Trim().Substring(2, 1));
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+#nullable restore
+}
