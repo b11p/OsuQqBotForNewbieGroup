e0aa267f65eb2dc018aeeb77b9e1ea13e6c2b282
bleatingsheep <bleatingsheep@live.cn>
2021-02-26 00:06:25 +0900

    Update Arily.

--- Bleatingsheep.NewHydrant.Bot/Osu/ArilyInfo.cs
+++ Bleatingsheep.NewHydrant.Bot/Osu/ArilyInfo.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Bleatingsheep.NewHydrant.Attributions;
 using Bleatingsheep.NewHydrant.啥玩意儿啊;
+using Bleatingsheep.Osu;
 using PuppeteerSharp;
 using Sisters.WudiLib;
 using Sisters.WudiLib.Posts;
@@ -12,33 +14,31 @@ using MessageContext = Sisters.WudiLib.Posts.Message;
 namespace Bleatingsheep.NewHydrant.Osu
 {
     [Component("arily")]
-    internal class ArilyBP : OsuFunction, IMessageCommand
+    internal class ArilyInfo : OsuFunction, IMessageCommand
     {
-        private static readonly Regex s_regex = new Regex(@"^\s*arily[1!！]{2}\s*(?<name>" + OsuHelper.UsernamePattern + @")?\s*(?:[，,]\s*(?<date>.*?))?\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+        private static readonly Regex s_regex = new Regex(@"^\s*arily[1!！]{2}\s*(?<name>" + OsuHelper.UsernamePattern + @")?\s*[,，]?\s*(?<mode>\S*)\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+        private static readonly IReadOnlyDictionary<Mode, string> s_modes = new Dictionary<Mode, string>
+        {// mode是['osu','taiko','fruits','mania']
+            { Mode.Standard, "osu" },
+            { Mode.Taiko, "taiko" },
+            { Mode.Catch, "fruits" },
+            { Mode.Mania, "mania" },
+        };
 
         private string _text;
 
         [Parameter("name")]
         public string Name { get; set; }
 
-        [Parameter("date")]
-        public string DateTimeText { get; set; }
+        [Parameter("mode")]
+        public string ModeString { get; set; }
 
         public async Task ProcessAsync(MessageContext context, HttpApiClient api)
         {
             int uid;
-            if (!string.IsNullOrEmpty(DateTimeText))
-            {
-                if (DateTime.TryParse(DateTimeText, out var date))
-                {
-                    DateTimeText = date.ToString("yyyy-M-d");
-                }
-                else
-                {
-                    //await api.SendMessageAsync(context.Endpoint, "日期格式错误").ConfigureAwait(false);
-                    //return;
-                }
-            }
+            Mode mode;
+            try { mode = ModeExtensions.Parse(ModeString); }
+            catch { mode = default; }
 
             if (string.IsNullOrEmpty(Name))
             {
@@ -46,20 +46,19 @@ namespace Bleatingsheep.NewHydrant.Osu
             }
             else
             {
-                var user = await EnsureGetUserInfo(Name, Bleatingsheep.Osu.Mode.Standard).ConfigureAwait(false);
+                var user = await EnsureGetUserInfo(Name, Mode.Standard).ConfigureAwait(false);
                 uid = user.Id;
             }
-            string url = $"https://o.ri.mk/users/{uid}/{DateTimeText}";
+            string url = $"https://info.osustuff.ri.mk/cn/users/{uid}/{s_modes.GetValueOrDefault(mode)}";
             using var page = await Chrome.OpenNewPageAsync().ConfigureAwait(false);
             await page.SetViewportAsync(new ViewPortOptions
             {
-                DeviceScaleFactor = Math.Sqrt(2),
-                Width = 700,
+                DeviceScaleFactor = Math.Sqrt(0.5),
+                Width = 1058,
                 Height = 80,
             }).ConfigureAwait(false);
-            _ = await page.GoToAsync(url).ConfigureAwait(false);
-            _ = await page.WaitForSelectorAsync("#finish").ConfigureAwait(false);
-            //await Task.Delay(0).ConfigureAwait(false);
+            _ = await page.GoToAsync(url, WaitUntilNavigation.Networkidle0).ConfigureAwait(false);
+            await page.WaitForTimeoutAsync(500).ConfigureAwait(false);
             var data = await page.ScreenshotDataAsync(new ScreenshotOptions
             {
                 FullPage = true,
