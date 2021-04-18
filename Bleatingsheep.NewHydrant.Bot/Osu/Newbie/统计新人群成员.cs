8c68b076b4c88df58cae042cbf037c2a3f2bf192
bleatingsheep <bleatingsheep@live.cn>
2021-04-18 14:33:25 +0900

    Update group member statistics.

--- Bleatingsheep.NewHydrant.Bot/Osu/Newbie/统计新人群成员.cs
+++ Bleatingsheep.NewHydrant.Bot/Osu/Newbie/统计新人群成员.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Bleatingsheep.NewHydrant.Attributions;
 using Bleatingsheep.Osu;
@@ -15,16 +17,38 @@ namespace Bleatingsheep.NewHydrant.Osu.Newbie
     [Component("newbie_statistics")]
     public class 统计新人群成员 : OsuFunction, IMessageCommand
     {
-        private const int 新人群号 = 885984366;
         private const int 管理群号 = 695600319;
-        private const int Limit = 2800;
-        private const string DestPath = "/root/outputs/statistics.csv";
-        private const string ResourceUrl = "https://res.bleatingsheep.org/statistics.csv";
+        private const string DestPath = "/root/outputs/statistics_{0}.csv";
+        private const string ResourceUrl = "https://res.bleatingsheep.org/statistics_{0}.csv";
 
-        public async Task ProcessAsync(MessageContext context, HttpApiClient api)
+        private static readonly Dictionary<string, (long, int)> s_groups = new()
         {
-            await api.SendMessageAsync(context.Endpoint, $"即将统计新人群玩家列表，PP 超过 {Limit} 将标记为超限。");
-            var infoList = await api.GetGroupMemberListAsync(新人群号);
+            { "新人群", (885984366, 2800) },
+            { "进阶群", (928936255, 4500) },
+        };
+
+        private static readonly Regex s_regex = new Regex("^统计(?<group>.+?)玩家$", RegexOptions.CultureInvariant | RegexOptions.Compiled);
+
+        [Parameter("group")]
+        public string ProcessingGroupName { get; set; }
+
+        public Task ProcessAsync(MessageContext context, HttpApiClient api)
+        {
+            if (s_groups.TryGetValue(ProcessingGroupName, out var t))
+            {
+                var (groupId, limit) = t;
+                return AnalyzeGroupMember(context, api, limit, groupId);
+            }
+            else
+            {
+                return Task.CompletedTask;
+            }
+        }
+
+        private async Task AnalyzeGroupMember(MessageContext context, HttpApiClient api, int limit, long groupId)
+        {
+            await api.SendMessageAsync(context.Endpoint, $"即将统计新人群玩家列表，PP 超过 {limit} 将标记为超限。");
+            var infoList = await api.GetGroupMemberListAsync(groupId);
             var results = new ConcurrentBag<string>();
             Parallel.ForEach(infoList, i =>
             {
@@ -51,14 +75,14 @@ namespace Bleatingsheep.NewHydrant.Osu.Newbie
                     else
                     {
                         results.Add($"{i.UserId},{result},{user.Name},{user.Performance},{JsonConvert.SerializeObject(i.DisplayName)}," +
-                            $"{(user.Performance == 0 ? "可能不活跃" : (user.Performance >= Limit ? "超限" : "正常"))}");
+                            $"{(user.Performance == 0 ? "可能不活跃" : (user.Performance >= limit ? "超限" : "正常"))}");
                     }
                 }
             });
             var message = string.Join("\r\n", results.Prepend("qq,uid,name,pp,card,remark"));
             Logger.Info(message);
-            File.WriteAllText(DestPath, message, new System.Text.UTF8Encoding(true));
-            await api.SendMessageAsync(context.Endpoint, $"统计完成，前往 {ResourceUrl} 查看结果。");
+            File.WriteAllText(string.Format(DestPath, groupId), message, new System.Text.UTF8Encoding(true));
+            await api.SendMessageAsync(context.Endpoint, $"统计完成，前往 {string.Format(ResourceUrl, groupId)} 查看结果。");
         }
 
         public bool ShouldResponse(MessageContext context)
@@ -66,7 +90,7 @@ namespace Bleatingsheep.NewHydrant.Osu.Newbie
             return context is GroupMessage g
                 && g.GroupId == 管理群号
                 && g.Content.TryGetPlainText(out string text)
-                && text.Trim() == "统计新人群玩家";
+                && RegexCommand(s_regex, text);
         }
     }
 }
