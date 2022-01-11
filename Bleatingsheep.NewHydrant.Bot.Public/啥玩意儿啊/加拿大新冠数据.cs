9110515ad0615666e836f65c8271582b87aa5c55
bleatingsheep <bleatingsheep@live.cn>
2022-01-11 16:35:08 +0900

    feat: Add US and Canada COVID cases data.

---
+++ Bleatingsheep.NewHydrant.Bot.Public/啥玩意儿啊/加拿大新冠数据.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bleatingsheep.NewHydrant.Attributions;
+using PuppeteerSharp;
+using Sisters.WudiLib;
+using Message = Sisters.WudiLib.SendingMessage;
+using MessageContext = Sisters.WudiLib.Posts.Message;
+
+namespace Bleatingsheep.NewHydrant.啥玩意儿啊;
+
+[Component("canada_covid_19")]
+internal class 加拿大新冠数据 : IMessageCommand
+{
+    public async Task ProcessAsync(MessageContext context, HttpApiClient api)
+    {
+        using var page = await Chrome.OpenNewPageAsync().ConfigureAwait(false);
+        await page.SetViewportAsync(new ViewPortOptions
+        {
+            DeviceScaleFactor = 3,
+            Width = 360,
+            Height = 8000,
+        }).ConfigureAwait(false);
+        await page.GoToAsync("https://en.wikipedia.org/wiki/Template:COVID-19_pandemic_data/Canada_medical_cases_by_province").ConfigureAwait(false);
+        var element = await page.QuerySelectorAsync("#mw-content-text > div.mw-parser-output > table").ConfigureAwait(false);
+        await page.SetViewportAsync(new ViewPortOptions
+        {
+            DeviceScaleFactor = 2,
+            Width = 1024,
+            Height = 4000,
+        }).ConfigureAwait(false);
+        var data2 = await element.ScreenshotDataAsync(new ScreenshotOptions
+        {
+            Type = ScreenshotType.Jpeg,
+            Quality = 100,
+        }).ConfigureAwait(false);
+        await api.SendMessageAsync(context.Endpoint, Message.ByteArrayImage(data2)).ConfigureAwait(false);
+    }
+
+    public bool ShouldResponse(MessageContext context)
+        => context.Content.TryGetPlainText(out var text) && "加拿大完了吗".Equals(text, StringComparison.Ordinal);
+}
---
+++ Bleatingsheep.NewHydrant.Bot.Public/啥玩意儿啊/美国新冠数据.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bleatingsheep.NewHydrant.Attributions;
+using PuppeteerSharp;
+using Sisters.WudiLib;
+using Message = Sisters.WudiLib.SendingMessage;
+using MessageContext = Sisters.WudiLib.Posts.Message;
+
+namespace Bleatingsheep.NewHydrant.啥玩意儿啊;
+
+[Component("us_covid_19")]
+internal class 美国新冠数据 : IMessageCommand
+{
+    public async Task ProcessAsync(MessageContext context, HttpApiClient api)
+    {
+        using var page = await Chrome.OpenNewPageAsync().ConfigureAwait(false);
+        await page.SetViewportAsync(new ViewPortOptions
+        {
+            DeviceScaleFactor = 3,
+            Width = 360,
+            Height = 8000,
+        }).ConfigureAwait(false);
+        await page.GoToAsync("https://www.nytimes.com/interactive/2021/us/covid-cases.html").ConfigureAwait(false);
+        await page.WaitForSelectorAsync("#us-covid-cases > div > div > main > div.g-columns-outer.svelte-hfvvmm > div:nth-child(2) > section:nth-child(1)").ConfigureAwait(false);
+        var deleteElement = await page.QuerySelectorAsync("#standalone-footer > div > div").ConfigureAwait(false);
+        await deleteElement.EvaluateFunctionAsync("b => b.remove()").ConfigureAwait(false);
+        var element = await page.QuerySelectorAsync("#us-covid-cases > div > div > main > div.g-columns-outer.svelte-hfvvmm > div:nth-child(2) > section:nth-child(1)").ConfigureAwait(false);
+        await page.SetViewportAsync(new ViewPortOptions
+        {
+            DeviceScaleFactor = 2,
+            Width = 1024,
+            Height = 4000,
+        }).ConfigureAwait(false);
+        var data2 = await element.ScreenshotDataAsync(new ScreenshotOptions
+        {
+            Type = ScreenshotType.Jpeg,
+            Quality = 100,
+        }).ConfigureAwait(false);
+        await api.SendMessageAsync(context.Endpoint, Message.ByteArrayImage(data2)).ConfigureAwait(false);
+    }
+
+    public bool ShouldResponse(MessageContext context)
+        => context.Content.TryGetPlainText(out var text) && "美国完了吗".Equals(text, StringComparison.Ordinal);
+}
