7bde9a4e5f593084112afab92af842f69053aede
bleatingsheep <bleatingsheep@live.cn>
2022-02-13 00:53:51 +0900

    Move test command.

---
+++ Bleatingsheep.NewHydrant.Bot.Private/Tests/LoadAvg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+using Bleatingsheep.NewHydrant.Attributions;
+using Sisters.WudiLib;
+using MessageContext = Sisters.WudiLib.Posts.Message;
+
+namespace Bleatingsheep.NewHydrant.Tests;
+
+[Component("loadavg")]
+class LoadAvg : IMessageCommand
+{
+    public async Task ProcessAsync(MessageContext context, HttpApiClient api)
+    {
+        var load = File.ReadAllText("/proc/loadavg");
+        var loadArray = load.Split();
+        var messageArray = new string[3];
+        messageArray[0] = $"{loadArray[0]} {loadArray[1]} {loadArray[2]}";
+        messageArray[1] = context.MessageId.ToString(CultureInfo.InvariantCulture);
+        messageArray[2] = context.Time.ToOffset(TimeSpan.FromHours(9)).ToString("H:mm:ss");
+        await api.SendMessageAsync(context.Endpoint, string.Join("\r\n", messageArray)).ConfigureAwait(false);
+    }
+
+    public bool ShouldResponse(MessageContext context)
+        => context.UserId == 962549599 && context.Content.TryGetPlainText(out var text) && "loadavg".Equals(text, StringComparison.OrdinalIgnoreCase);
+}
