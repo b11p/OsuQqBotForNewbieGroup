f9b8bd5f60715d4a067ebff61ca8105ad7120675
bleatingsheep <bleatingsheep@live.cn>
2022-07-12 01:28:54 +0800

    check not scheduled newbie member.

--- Bleatingsheep.NewHydrant.Bot.Private/Osu/Newbie/统计新人群成员.cs
+++ Bleatingsheep.NewHydrant.Bot.Private/Osu/Newbie/统计新人群成员.cs
@@ -11,6 +11,7 @@ using Bleatingsheep.NewHydrant.Data;
 using Bleatingsheep.Osu;
 using Bleatingsheep.OsuQqBot.Database.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Sisters.WudiLib;
 using Sisters.WudiLib.Posts;
 using MessageContext = Sisters.WudiLib.Posts.Message;
@@ -32,13 +33,15 @@ namespace Bleatingsheep.NewHydrant.Osu.Newbie
 
         private static readonly Regex s_regex = new Regex("^统计(?<group>.+?)玩家$", RegexOptions.CultureInvariant | RegexOptions.Compiled);
 
-        private readonly Lazy<NewbieContext> _lazyContext;
+        private readonly IDbContextFactory<NewbieContext> _contextFactory;
         private readonly Lazy<IDataProvider> _dataProvider;
+        private readonly ILogger<统计新人群成员> _logger;
 
-        public 统计新人群成员(Lazy<NewbieContext> newbieContext, Lazy<IDataProvider> dataProvider)
+        public 统计新人群成员(IDbContextFactory<NewbieContext> newbieContext, Lazy<IDataProvider> dataProvider, ILogger<统计新人群成员> logger)
         {
-            _lazyContext = newbieContext;
+            _contextFactory = newbieContext;
             _dataProvider = dataProvider;
+            _logger = logger;
         }
 
         [Parameter("group")]
@@ -60,16 +63,19 @@ namespace Bleatingsheep.NewHydrant.Osu.Newbie
         private async Task AnalyzeGroupMember(MessageContext context, HttpApiClient api, int limit, long groupId)
         {
             await api.SendMessageAsync(context.Endpoint, $"即将统计新人群玩家列表，PP 超过 {limit} 将标记为超限。");
+
+            var dbContext = _contextFactory.CreateDbContext();
+            dbContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
             var infoList = await api.GetGroupMemberListAsync(groupId);
             Logger.Info($"Find {infoList.Length} users.");
             var userIdList = infoList.Select(i => i.UserId).ToList();
-            IQueryable<BindingInfo> bindingQuery = _lazyContext.Value.Bindings.Where(b => userIdList.Contains(b.UserId));
+            IQueryable<BindingInfo> bindingQuery = dbContext.Bindings.Where(b => userIdList.Contains(b.UserId));
             var bindingList = await bindingQuery.ToListAsync().ConfigureAwait(false);
             Logger.Info($"Find {bindingList.Count} binding info.");
 
             var osuIds = bindingList.Select(b => b.OsuId).Distinct().ToList();
             var snapshotNoEarlierThan = DateTime.UtcNow.AddDays(-1);
-            var snapshots = await _lazyContext.Value.UserSnapshots
+            var snapshots = await dbContext.UserSnapshots
                 .Where(s => s.Date > snapshotNoEarlierThan && osuIds.Contains(s.UserId) && s.Mode == Mode.Standard)
                 .AsAsyncEnumerable()
                 .GroupBy(s => s.UserId)
@@ -122,6 +128,28 @@ namespace Bleatingsheep.NewHydrant.Osu.Newbie
             Logger.Info(message);
             File.WriteAllText(string.Format(DestPath, groupId), message, new System.Text.UTF8Encoding(true));
             await api.SendMessageAsync(context.Endpoint, $"统计完成，前往 {string.Format(ResourceUrl, groupId)} 查看结果。");
+
+            // add users not having snapshots to schedule
+            // get scheduled users
+            var scheduled = await dbContext.UpdateSchedules
+                .Where(s => s.Mode == Mode.Standard && osuIds.Contains(s.UserId))
+                .Select(s => s.UserId)
+                .ToListAsync().ConfigureAwait(false);
+            // get users not scheduled
+            var notScheduled = osuIds.Except(scheduled).ToList();
+            Logger.Info($"{notScheduled.Count} users not scheduled to create snapshots.");
+            // add users not scheduled
+            if (notScheduled.Count > 0)
+            {
+                var newSchedules = notScheduled.Select(u => new UpdateSchedule
+                {
+                    UserId = u,
+                    Mode = Mode.Standard,
+                    NextUpdate = DateTime.UtcNow,
+                });
+                dbContext.UpdateSchedules.AddRange(newSchedules);
+                await dbContext.SaveChangesAsync().ConfigureAwait(false);
+            }
         }
 
         public bool ShouldResponse(MessageContext context)
