e05308e187092096e27e9e55e04bdb5599cc4195
bleatingsheep <bleatingsheep@live.cn>
2022-12-21 21:27:27 -0500

    feat (myyearly): add more stat.

--- Bleatingsheep.NewHydrant.Bot.Private/Osu/Yearly/MyYearly.cs
+++ Bleatingsheep.NewHydrant.Bot.Private/Osu/Yearly/MyYearly.cs
@@ -5,6 +5,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Bleatingsheep.NewHydrant.Attributions;
 using Bleatingsheep.NewHydrant.Data;
+using Bleatingsheep.Osu;
 using Bleatingsheep.Osu.ApiClient;
 using Bleatingsheep.OsuQqBot.Database.Models;
 using Microsoft.EntityFrameworkCore;
@@ -22,10 +23,11 @@ namespace Bleatingsheep.NewHydrant.Osu.Yearly
         private readonly IDataProvider _dataProvider;
         private readonly IOsuApiClient _osuApiClient;
         private readonly NewbieContext _newbieContext;
-        private Mode _mode = Mode.Standard;
         private readonly TimeSpan _timeZone = TimeSpan.FromHours(8);
 
-        private IReadOnlyList<UserPlayRecord> _userPlayRecords = Array.Empty<UserPlayRecord>();
+        private Mode _mode = Mode.Standard;
+        private IReadOnlyList<UserPlayRecord> _userPlayRecords = default!;
+        private IReadOnlyDictionary<int, BeatmapInfo> _beatmapInfoDict = default!;
 
         public MyYearly(IDataProvider dataProvider, IOsuApiClient osuApiClient, NewbieContext newbieContext)
         {
@@ -34,7 +36,7 @@ namespace Bleatingsheep.NewHydrant.Osu.Yearly
             _newbieContext = newbieContext;
         }
 
-        private string ModeString { get; set; }
+        private string ModeString { get; set; } = default!;
 
         public async Task ProcessAsync(MessageContext context, HttpApiClient api)
         {
@@ -51,7 +53,10 @@ namespace Bleatingsheep.NewHydrant.Osu.Yearly
             // apply mode
             try
             {
-                _mode = Bleatingsheep.Osu.ModeExtensions.Parse(ModeString);
+                if (!string.IsNullOrWhiteSpace(ModeString))
+                {
+                    _mode = ModeExtensions.Parse(ModeString);
+                }
             }
             catch
             {
@@ -85,20 +90,21 @@ namespace Bleatingsheep.NewHydrant.Osu.Yearly
                 .Where(r => r.UserId == osuId && r.Mode == _mode && r.PlayNumber > startPC)
                 .OrderBy(r => r.PlayNumber)
                 .ToListAsync().ConfigureAwait(false);
-            _ = await api.SendMessageAsync(context.Endpoint, $"当前模式：{_mode}数据完整度：{playList.Count} of {currentPC - startPC}。功能制作中。正在生成报告，请稍候。").ConfigureAwait(false);
+            _ = await api.SendMessageAsync(context.Endpoint, $"当前模式：{_mode}，数据完整度：{playList.Count} of {currentPC - startPC}。功能制作中。正在生成报告，请稍候。").ConfigureAwait(false);
             if (playList.Count == 0)
+            {
+                await api.SendMessageAsync(context.Endpoint, "你在过去一年没有玩儿过 osu!，或无数据记录。").ConfigureAwait(false);
                 return;
+            }
 
-            // favorite mapper
+            // beatmap info
             var playedBeatmaps = playList.Select(r => r.Record.BeatmapId).Distinct().ToHashSet();
             var cachedBeatmapInfo = await _newbieContext.BeatmapInfoCache.Where(e => e.Mode == _mode && playedBeatmaps.Contains(e.BeatmapId)).ToListAsync().ConfigureAwait(false);
-            var remainingBeatmaps = playedBeatmaps.Except(cachedBeatmapInfo.Select(e => e.BeatmapInfo.Id));
+            var noCacheBeatmaps = playedBeatmaps.Except(cachedBeatmapInfo.Select(e => e.BeatmapInfo.Id));
             var beatmapInfoList = cachedBeatmapInfo.ConvertAll(e => e.BeatmapInfo);
-            string? favoriteMapperName = default;
-            int favoriteMapperPlayCount = default;
-            var mapperTask = Task.Run(async () =>
+            try
             {
-                foreach (var beatmapId in remainingBeatmaps)
+                foreach (var beatmapId in noCacheBeatmaps)
                 {
                     var current = await _dataProvider.GetBeatmapInfoAsync(beatmapId, _mode).ConfigureAwait(false);
                     if (current != null)
@@ -106,24 +112,11 @@ namespace Bleatingsheep.NewHydrant.Osu.Yearly
                         beatmapInfoList.Add(current);
                     }
                 }
-                var beatmapToCreatorDic = beatmapInfoList.ToDictionary(i => i.Id, i => i.CreatorId);
-                var favoriteMapperId = playedBeatmaps.GroupBy(bid => beatmapToCreatorDic.GetValueOrDefault(bid)).OrderByDescending(g => g.Count()).SkipWhile(g => g.Key == 0).FirstOrDefault()?.Key;
-                if (favoriteMapperId != null)
-                {
-                    var favoriteMapperBeatmaps = beatmapInfoList.Where(b => b.CreatorId == favoriteMapperId).ToList();
-                    favoriteMapperName = favoriteMapperBeatmaps.OrderByDescending(b => b.ApprovedDate).FirstOrDefault()?.Creator;
-                    var favoriteMapperBeatmapIds = favoriteMapperBeatmaps.Select(b => b.Id).ToHashSet();
-                    favoriteMapperPlayCount = playList.Count(r => favoriteMapperBeatmapIds.Contains(r.Record.BeatmapId));
-                }
-            });
-            try
-            {
-                // favorite mapper
-                await mapperTask.ConfigureAwait(false);
             }
             catch
             {
             }
+            _beatmapInfoDict = beatmapInfoList.ToDictionary(bi => bi.Id);
 
             // assign data to fields.
             _userPlayRecords = playList;
@@ -139,6 +132,12 @@ namespace Bleatingsheep.NewHydrant.Osu.Yearly
                     $"{beatmap}").ConfigureAwait(false);
             }
             {
+                // mods
+                var (mods, count) = GetFavoriteMods();
+                _ = await api.SendMessageAsync(context.Endpoint, $"你最喜欢的 mods 是 {mods.Display()}，贡献了你 {(double)count / _userPlayRecords.Count:P0} 的游玩次数。").ConfigureAwait(false);
+            }
+            {
+                (string? favoriteMapperName, int favoriteMapperPlayCount) = GetFavoriteMapper();
                 if (favoriteMapperName != null)
                 {
                     _ = await api.SendMessageAsync(context.Endpoint, $"你最喜欢的 mapper 是 {favoriteMapperName}，打了她/他的图 {favoriteMapperPlayCount} 次。").ConfigureAwait(false);
@@ -148,6 +147,17 @@ namespace Bleatingsheep.NewHydrant.Osu.Yearly
                 // most playing hour
                 var mostPlayingHour = GetMostPlayingHours();
                 _ = await api.SendMessageAsync(context.Endpoint, $"你最常在 {mostPlayingHour}-{mostPlayingHour + 1} 时打图。").ConfigureAwait(false);
+            }
+            {
+                // most played beatmap of the day
+                var (bid, date, count, fc) = GetMostPlayedBeatmapOfDay();
+                string fcString = fc == true
+                    ? "全连了，真不容易。"
+                    : fc == false
+                    ? "都没全连，真菜。"
+                    : string.Empty;
+                var beatmapInfo = _beatmapInfoDict.GetValueOrDefault(bid);
+                await api.SendMessageAsync(context.Endpoint, $"{date.ToShortDateString()}，你把 {bid} 打了 {count} 次。{fcString}（{beatmapInfo}）").ConfigureAwait(false);
             }
         }
 
@@ -172,6 +182,12 @@ namespace Bleatingsheep.NewHydrant.Osu.Yearly
             return (bid, count, beatmap);
         }
 
+        private (Mods mods, int count) GetFavoriteMods()
+        {
+            var favModsGroup = _userPlayRecords.GroupBy(r => r.Record.EnabledMods).OrderByDescending(g => g.Count()).First();
+            return (favModsGroup.Key, favModsGroup.Count());
+        }
+
         private int GetMostPlayingHours()
         {
             int mostPlayingHour = _userPlayRecords
@@ -180,6 +196,39 @@ namespace Bleatingsheep.NewHydrant.Osu.Yearly
                 .First()
                 .Key;
             return mostPlayingHour;
+        }
+
+        private (string? favoriteMapperName, int favoriteMapperPlayCount) GetFavoriteMapper()
+        {
+            var playedBeatmaps = _userPlayRecords.Select(r => r.Record.BeatmapId).Distinct().ToHashSet();
+            var favoriteMapperId = playedBeatmaps.GroupBy(bid => _beatmapInfoDict.GetValueOrDefault(bid)?.CreatorId).OrderByDescending(g => g.Count()).FirstOrDefault(g => g.Key != default)?.Key;
+            if (favoriteMapperId != null)
+            {
+                var favoriteMapperBeatmaps = _beatmapInfoDict.Values.Where(b => b.CreatorId == favoriteMapperId).ToList();
+                var favoriteMapperName = favoriteMapperBeatmaps.OrderByDescending(b => b.ApprovedDate).FirstOrDefault()?.Creator;
+                var favoriteMapperBeatmapIds = favoriteMapperBeatmaps.Select(b => b.Id).ToHashSet();
+                var favoriteMapperPlayCount = _userPlayRecords.Count(r => favoriteMapperBeatmapIds.Contains(r.Record.BeatmapId));
+                return (favoriteMapperName, favoriteMapperPlayCount);
+            }
+            return default;
+        }
+
+        private (int beatmapId, DateTime date, int count, bool? fullCombo) GetMostPlayedBeatmapOfDay()
+        {
+            var mostPlayedOfTheDay = _userPlayRecords.GroupBy(r =>
+            {
+                var adjustedDate = new DateTimeOffset(r.Record.Date).ToOffset(_timeZone).AddHours(-5).Date;
+                return (r.Record.BeatmapId, adjustedDate);
+            }).OrderByDescending(g => g.Count()).First();
+            var (bid, date) = mostPlayedOfTheDay.Key;
+            var beatmapInfo = _beatmapInfoDict.GetValueOrDefault(bid);
+            bool? fullCombo = default;
+            if (beatmapInfo != null)
+            {
+                var maxCombo = beatmapInfo.MaxCombo;
+                fullCombo = mostPlayedOfTheDay.Any(r => r.Record.CountMiss == 0 && r.Record.Count100 + r.Record.Count50 > maxCombo - r.Record.MaxCombo);
+            }
+            return (bid, date, mostPlayedOfTheDay.Count(), fullCombo);
         }
 
         [GeneratedRegex("^我的年度(?:屙屎|osu[!！]?)\\s*(?:[,，]\\s*(?<mode>.+?)\\s*)?$", RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase)]
