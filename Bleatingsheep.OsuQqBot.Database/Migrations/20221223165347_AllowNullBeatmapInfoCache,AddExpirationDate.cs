44141f202d3b8711fe189b6eb7c1934928478b25
bleatingsheep <bleatingsheep@live.cn>
2022-12-23 11:55:40 -0500

    db: allow null beatmap info cache; add expiration date.

---
+++ Bleatingsheep.OsuQqBot.Database/Migrations/20221223165347_AllowNullBeatmapInfoCache,AddExpirationDate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Bleatingsheep.Osu.ApiClient;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bleatingsheep.OsuQqBot.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllowNullBeatmapInfoCacheAddExpirationDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<BeatmapInfo>(
+                name: "BeatmapInfo",
+                table: "BeatmapInfoCache",
+                type: "jsonb",
+                nullable: true,
+                oldClrType: typeof(BeatmapInfo),
+                oldType: "jsonb");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ExpirationDate",
+                table: "BeatmapInfoCache",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BeatmapInfoCache_ExpirationDate",
+                table: "BeatmapInfoCache",
+                column: "ExpirationDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BeatmapInfoCache_ExpirationDate",
+                table: "BeatmapInfoCache");
+
+            migrationBuilder.DropColumn(
+                name: "ExpirationDate",
+                table: "BeatmapInfoCache");
+
+            migrationBuilder.AlterColumn<BeatmapInfo>(
+                name: "BeatmapInfo",
+                table: "BeatmapInfoCache",
+                type: "jsonb",
+                nullable: false,
+                oldClrType: typeof(BeatmapInfo),
+                oldType: "jsonb",
+                oldNullable: true);
+        }
+    }
+}
--- Bleatingsheep.OsuQqBot.Database/Models/BeatmapInfoCacheEntry.cs
+++ Bleatingsheep.OsuQqBot.Database/Models/BeatmapInfoCacheEntry.cs
@@ -2,15 +2,18 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using Bleatingsheep.Osu;
 using Bleatingsheep.Osu.ApiClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bleatingsheep.OsuQqBot.Database.Models;
 #nullable enable
+[Index(nameof(ExpirationDate))]
 public class BeatmapInfoCacheEntry
 {
     public int BeatmapId { get; set; }
     public Mode Mode { get; set; }
     public DateTimeOffset CacheDate { get; set; }
+    public DateTimeOffset? ExpirationDate { get; set; }
     [Column(TypeName = "jsonb")]
-    public required BeatmapInfo BeatmapInfo { get; set; }
+    public required BeatmapInfo? BeatmapInfo { get; set; }
 }
 #nullable restore
