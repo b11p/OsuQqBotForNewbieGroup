3d808ed18120b4351b90876d66eaf303e8d83df8
bleatingsheep <bleatingsheep@live.cn>
2023-01-14 22:27:38 -0500

    feat: (wip) add maintenance.

---
+++ Bleatingsheep.NewHydrant.DataMaintenance/Program.cs
@@ -0,0 +1,26 @@
+using Bleatingsheep.NewHydrant.DataMaintenance;
+using Bleatingsheep.OsuQqBot.Database.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((ctx, services) =>
+    {
+        services.AddHostedService<Worker>();
+        services.AddDbContext<NewbieContext>(optionsBuilder =>
+            optionsBuilder.UseNpgsql(
+                ctx.Configuration.GetConnectionString("NewbieDatabase_Postgres"),
+                options => options.EnableRetryOnFailure())
+                .ConfigureWarnings(c => c.Log((RelationalEventId.CommandExecuting, LogLevel.Debug),
+                    (RelationalEventId.CommandExecuted, LogLevel.Debug))),
+            ServiceLifetime.Transient);
+        services.AddDbContextFactory<NewbieContext>(optionsBuilder =>
+            optionsBuilder.UseNpgsql(
+                ctx.Configuration.GetConnectionString("NewbieDatabase_Postgres"),
+                options => options.EnableRetryOnFailure())
+                .ConfigureWarnings(c => c.Log((RelationalEventId.CommandExecuting, LogLevel.Debug),
+                    (RelationalEventId.CommandExecuted, LogLevel.Debug))));
+    })
+    .Build();
+
+await host.RunAsync().ConfigureAwait(false);
---
+++ Bleatingsheep.NewHydrant.DataMaintenance/SyncSchedule.cs
@@ -0,0 +1,71 @@
+﻿using Bleatingsheep.Osu;
+using Bleatingsheep.OsuQqBot.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bleatingsheep.NewHydrant.DataMaintenance;
+public sealed class SyncSchedule : BackgroundService
+{
+    private static readonly SemaphoreSlim s_semaphore = new(1);
+    private readonly IDbContextFactory<NewbieContext> _dbContextFactory;
+    private readonly ILogger<SyncSchedule> _logger;
+
+    public static TimeSpan OnUtc => new(19, 29, 59);
+
+    public SyncSchedule(IDbContextFactory<NewbieContext> dbContextFactory, ILogger<SyncSchedule> logger)
+    {
+        _dbContextFactory = dbContextFactory;
+        _logger = logger;
+    }
+
+    public async Task RunAsync()
+    {
+        var db1 = _dbContextFactory.CreateDbContext();
+        await using var dispose1_ = db1.ConfigureAwait(false);
+        var snapshotted =
+            await db1.UserSnapshots
+            .Select(s => new { s.UserId, s.Mode })
+            .Distinct()
+            .ToListAsync()
+            .ConfigureAwait(false);
+        var binded = await
+            (from b in db1.Bindings.AsAsyncEnumerable()
+             from m in new[] { Mode.Standard, Mode.Taiko, Mode.Catch, Mode.Mania }.ToAsyncEnumerable()
+             select new { UserId = b.OsuId, Mode = m })
+            .ToListAsync().ConfigureAwait(false);
+        var scheduled =
+            await db1.UpdateSchedules
+            .Select(s => new { s.UserId, s.Mode })
+            .ToListAsync()
+            .ConfigureAwait(false);
+        var toSchedule = snapshotted.Intersect(binded).Except(scheduled).Select(i => new UpdateSchedule
+        {
+            UserId = i.UserId,
+            Mode = i.Mode,
+            NextUpdate = DateTimeOffset.UtcNow,
+        }).ToList();
+        if (toSchedule.Count > 0)
+        {
+            _logger.LogDebug("Adding {toSchedule.Count} items to schedule.", toSchedule.Count);
+            db1.UpdateSchedules.AddRange(toSchedule);
+            await db1.SaveChangesAsync().ConfigureAwait(false);
+        }
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var timeToNextRun = new TimeSpan(24, 0, 0) - (DateTimeOffset.UtcNow - OnUtc).TimeOfDay;
+            _logger.LogInformation("Will trigger at {OnUtc}, waiting {timeToNextRun}", OnUtc, timeToNextRun);
+            await Task.Delay(timeToNextRun, stoppingToken).ConfigureAwait(false);
+            try
+            {
+                await RunAsync().ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "error");
+            }
+        }
+    }
+}
