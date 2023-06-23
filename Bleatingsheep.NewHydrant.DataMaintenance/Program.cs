d0674618a03e82beca6fb92fbb5ec1daad3e493d
bleatingsheep <bleatingsheep@live.cn>
2023-06-23 16:25:00 -0400

    fix (maintenance): missing services.

--- Bleatingsheep.NewHydrant.DataMaintenance/Program.cs
+++ Bleatingsheep.NewHydrant.DataMaintenance/Program.cs
@@ -1,7 +1,10 @@
+using Bleatingsheep.NewHydrant.Data;
 using Bleatingsheep.NewHydrant.DataMaintenance;
+using Bleatingsheep.Osu.ApiClient;
 using Bleatingsheep.OsuQqBot.Database.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using WebApiClient;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((ctx, services) =>
@@ -23,6 +26,11 @@ IHost host = Host.CreateDefaultBuilder(args)
                 options => options.EnableRetryOnFailure())
                 .ConfigureWarnings(c => c.Log((RelationalEventId.CommandExecuting, LogLevel.Debug),
                     (RelationalEventId.CommandExecuted, LogLevel.Debug))));
+        services.AddTransient<DataMaintainer>();
+
+        var factory = OsuApiClientFactory.CreateFactory(ctx.Configuration.GetSection("Hydrant")["ApiKey"]);
+        services.AddSingleton<IHttpApiFactory<IOsuApiClient>>(factory);
+        services.AddScoped(c => c.GetRequiredService<IHttpApiFactory<IOsuApiClient>>().CreateHttpApi());
     })
     .Build();
 
