5dd6731d6092b31a68fb09c93a55aa2e0509cdcc
bleatingsheep <bleatingsheep@live.cn>
2023-06-23 22:08:58 -0400

    fix: recommendation data collection memory leak.

--- Bleatingsheep.NewHydrant.Bot.Private/Osu/Recommendations/RetriveRecommendationData.cs
+++ Bleatingsheep.NewHydrant.Bot.Private/Osu/Recommendations/RetriveRecommendationData.cs
@@ -10,9 +10,8 @@ using Bleatingsheep.Osu.ApiClient;
 using Bleatingsheep.OsuQqBot.Database.Models;
 using Microsoft.EntityFrameworkCore;
 using Sisters.WudiLib;
-using Message = Sisters.WudiLib.SendingMessage;
-using MessageContext = Sisters.WudiLib.Posts.Message;
 using static Bleatingsheep.Osu.Mods;
+using MessageContext = Sisters.WudiLib.Posts.Message;
 
 namespace Bleatingsheep.NewHydrant.Osu.Recommendations
 {
@@ -33,15 +32,16 @@ namespace Bleatingsheep.NewHydrant.Osu.Recommendations
         private static int errorCount = 0;
         private readonly IDataProvider _dataProvider;
         private readonly NewbieContext _newbieContext;
-
+        private readonly IDbContextFactory<NewbieContext> _dbContextFactory;
         private bool _start = false;
         private HttpApiClient _api = default!;
         private MessageContext _context = default!;
 
-        public RetriveRecommendationData(IDataProvider dataProvider, NewbieContext newbieContext)
+        public RetriveRecommendationData(IDataProvider dataProvider, NewbieContext newbieContext, IDbContextFactory<NewbieContext> dbContextFactory)
         {
             _dataProvider = dataProvider;
             _newbieContext = newbieContext;
+            _dbContextFactory = dbContextFactory;
         }
 
         public async Task ProcessAsync(MessageContext context, HttpApiClient api)
@@ -137,35 +137,16 @@ namespace Bleatingsheep.NewHydrant.Osu.Recommendations
                     recent = current;
                 }
             }
-            var recommendations = recommendationList.ToArray();
-            //await _newbieContext.Database.CreateExecutionStrategy().ExecuteAsync(async () =>
-            //{
-            //    await using var transaction = await _newbieContext.Database.BeginTransactionAsync(System.Data.IsolationLevel.Serializable).ConfigureAwait(false);
-            //var oldData = await _newbieContext.Recommendations.ToListAsync().ConfigureAwait(false);
-            //_newbieContext.Recommendations.RemoveRange(oldData);
-            //await _newbieContext.SaveChangesAsync().ConfigureAwait(false);
-            for (int i = 0; i * 10000 < recommendations.Length; i++)
+            var recommendationsChunks = recommendationList.Chunk(10000);
+            foreach (var chunk in recommendationsChunks)
             {
                 // 不这么干不行，因为 EF Core 追踪实体需要消耗大量内存。
                 // 现在这样可以把同时追踪的实体数限制在一万，消耗大约 600M 内存。
-                var toAdd = (i + 1) * 10000 > recommendations.Length
-                    ? recommendations[(i * 10000)..]
-                    : recommendations[(i * 10000)..((i + 1) * 10000)];
-                _newbieContext.Recommendations.AddRange(toAdd);
-                await _newbieContext.SaveChangesAsync().ConfigureAwait(false);
-
-                var changedEntriesCopy = _newbieContext.ChangeTracker.Entries()
-                    .Where(e => e.State == EntityState.Added ||
-                                e.State == EntityState.Modified ||
-                                e.State == EntityState.Deleted)
-                    .ToList();
-                foreach (var entry in changedEntriesCopy)
-                    entry.State = EntityState.Detached;
+                var toAdd = chunk;
+                await using var db = _dbContextFactory.CreateDbContext();
+                db.Recommendations.AddRange(toAdd);
+                await db.SaveChangesAsync();
             }
-            //_newbieContext.Recommendations.AddRange(recommendationList);
-            //await _newbieContext.SaveChangesAsync().ConfigureAwait(false);
-            //    await transaction.CommitAsync().ConfigureAwait(false);
-            //}).ConfigureAwait(false);
         }
 
         public bool ShouldResponse(MessageContext context)
