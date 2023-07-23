8344790151cd08fd901218b3db426d3c1920dadd
bleatingsheep <bleatingsheep@live.cn>
2023-07-23 11:39:18 -0400

    refactor: 随机数使用外部 API。

--- Bleatingsheep.NewHydrant.Bot.Public/啥玩意儿啊/真随机数.cs
+++ Bleatingsheep.NewHydrant.Bot.Public/啥玩意儿啊/真随机数.cs
@@ -1,7 +1,15 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Security.Cryptography;
+using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Bleatingsheep.NewHydrant.Attributions;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
 using Sisters.WudiLib;
 using MessageContext = Sisters.WudiLib.Posts.Message;
 
@@ -11,6 +19,14 @@ namespace Bleatingsheep.NewHydrant.啥玩意儿啊;
 internal partial class 真随机数 : IMessageCommand
 {
     private Match _match = default!;
+    private readonly IMemoryCache _memoryCache;
+    private readonly IConfiguration _configuration;
+
+    public 真随机数(IMemoryCache memoryCache, IConfiguration configuration)
+    {
+        _memoryCache = memoryCache;
+        _configuration = configuration;
+    }
 
     public async Task ProcessAsync(MessageContext context, HttpApiClient api)
     {
@@ -52,8 +68,85 @@ internal partial class 真随机数 : IMessageCommand
                 end = num2;
             }
         }
-        var val = RandomNumberGenerator.GetInt32(start, end);
-        await api.SendMessageAsync(context.Endpoint, val.ToString());
+        var val = await GetInt32(start, end);
+        await api.SendMessageAsync(context.Endpoint, $"通过量子力学为你生成的随机数是 {val}。");
+    }
+
+    private async ValueTask<int> GetInt32(int fromInclusive, int toExclusive)
+    {
+        Debug.Assert(fromInclusive < toExclusive);
+        var apiKey = _configuration["Services:random.org"];
+
+        // The total possible range is [0, 4,294,967,295).
+        // Subtract one to account for zero being an actual possibility.
+        uint range = (uint)toExclusive - (uint)fromInclusive - 1;
+
+        // If there is only one possible choice, nothing random will actually happen, so return
+        // the only possibility.
+        if (range == 0)
+        {
+            return fromInclusive;
+        }
+
+        using var http = new HttpClient();
+        using var response = await http.PostAsJsonAsync("https://api.random.org/json-rpc/4/invoke", new
+        {
+            jsonrpc = "2.0",
+            method = "generateIntegers",
+            @params = new
+            {
+                apiKey,
+                n = 1,
+                min = 0,
+                max = range,
+                replacement = true,
+            },
+            id = RandomNumberGenerator.GetInt32(0x40000000),
+        });
+        response.EnsureSuccessStatusCode();
+        var r = await response.Content.ReadFromJsonAsync<RandomNumberResponse>();
+        return (int)r!.Result.Random.Data[0] + fromInclusive;
+    }
+
+    public partial class RandomNumberResponse
+    {
+        [JsonPropertyName("jsonrpc")]
+        public required string Jsonrpc { get; set; }
+
+        [JsonPropertyName("result")]
+        public required Result Result { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+    }
+
+    public sealed class Result
+    {
+        [JsonPropertyName("random")]
+        public required RandomData Random { get; set; }
+
+        [JsonPropertyName("bitsUsed")]
+        public int BitsUsed { get; set; }
+
+        [JsonPropertyName("bitsLeft")]
+        public int BitsLeft { get; set; }
+
+        [JsonPropertyName("requestsLeft")]
+        public int RequestsLeft { get; set; }
+
+        [JsonPropertyName("advisoryDelay")]
+        public int AdvisoryDelay { get; set; }
+    }
+
+    public sealed class RandomData
+    {
+        [JsonPropertyName("data")]
+        public required List<uint> Data { get; set; }
+
+        // This field is of format: 2023-07-23 15:50:01Z
+        // However, System.Text.Json expects 2023-07-23T15:50:01Z
+        //[JsonPropertyName("completionTime")]
+        //public DateTimeOffset CompletionTime { get; set; }
     }
 
     public bool ShouldResponse(MessageContext context)
