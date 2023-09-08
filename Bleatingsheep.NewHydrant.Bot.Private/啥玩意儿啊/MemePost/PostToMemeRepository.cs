7b317df60f097539577610c34c6fea7b64857605
bleatingsheep <35132976+b11p@users.noreply.github.com>
2023-09-08 14:23:50 -0400

    fix: 增加meme禁用字符

--- Bleatingsheep.NewHydrant.Bot.Private/啥玩意儿啊/MemePost/PostToMemeRepository.cs
+++ Bleatingsheep.NewHydrant.Bot.Private/啥玩意儿啊/MemePost/PostToMemeRepository.cs
@@ -24,7 +24,7 @@ namespace Bleatingsheep.NewHydrant.啥玩意儿啊.MemePost;
 internal partial class PostToMemeRepository : IMessageCommand
 {
     private static readonly char[] s_invalidFileNameChars = Path.GetInvalidFileNameChars();
-    private static readonly char[] s_invalidFileNameCharsExtra = { '?', '\'', '"', '*' };
+    private static readonly char[] s_invalidFileNameCharsExtra = { '?', '\'', '"', '*', '#' };
     private static readonly string s_invalidFileNameCharsWindows = "\u0022\u003C\u003E|\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\b\t\n\u000B\f\r\u000E\u000F\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001A\u001B\u001C\u001D\u001E\u001F:*?\\/";
 
     private readonly IDbContextFactory<NewbieContext> _dbContextFactory;
