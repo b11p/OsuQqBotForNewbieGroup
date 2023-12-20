be989f4a1af00bcd8470b60e4701ba9926db3d6b
bleatingsheep <bleatingsheep@live.cn>
2023-12-19 23:06:52 -0500

    feat: bump all to .net 8

--- NewHydrantApi/Startup.cs
+++ NewHydrantApi/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using IPNetwork = Microsoft.AspNetCore.HttpOverrides.IPNetwork;
 
 namespace NewHydrantApi
 {
