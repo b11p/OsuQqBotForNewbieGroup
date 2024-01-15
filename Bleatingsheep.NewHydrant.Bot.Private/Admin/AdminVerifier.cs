4f03c54d91ecf70d2d766e119e72186859f2fa13
HollisMeynell <56613764+HollisMeynell@users.noreply.github.com>
2024-01-15 19:26:14 +0800

    新管理(去年的没加

--- Bleatingsheep.NewHydrant.Bot.Private/Admin/AdminVerifier.cs
+++ Bleatingsheep.NewHydrant.Bot.Private/Admin/AdminVerifier.cs
@@ -32,6 +32,10 @@ namespace Bleatingsheep.NewHydrant.Admin
             630060047, // CYCLC
             365246692, // -Spring Night-
             1120180945, // n0000000000o
+            2199188467, // NatsuRin
+            524986802, // Dragon-Fox
+            411843675, // xxbg
+            1584775323. // YRScarlet
         };
 
         public AdminVerifier()
