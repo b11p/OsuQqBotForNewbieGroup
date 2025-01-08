072fda48cb9cc4ec8359fed6df6109385d8a6134
bleatingsheep <bleatingsheep@live.cn>
2025-01-08 19:49:51 +0800

    fix: 缩短拒绝提示长度

--- Bleatingsheep.NewHydrant.Bot.Private/Osu/Newbie/NotifyOnJoinRequest.cs
+++ Bleatingsheep.NewHydrant.Bot.Private/Osu/Newbie/NotifyOnJoinRequest.cs
@@ -254,7 +254,7 @@ namespace Bleatingsheep.NewHydrant.Osu.Newbie
                 var (performance, level) = HintBinding(httpApiClient, endpoint, e).ConfigureAwait(false).GetAwaiter().GetResult();
                 if (performance >= limit)
                 {
-                    string response = "您的 PP 高于此群大部分玩家，不推荐加入本群。请加入";
+                    string response = "您的 PP 过高，请加入";
                     string groups = "其他群";
                     string reason = "暂无";
 
