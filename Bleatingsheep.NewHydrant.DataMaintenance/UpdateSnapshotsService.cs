272f1073884db2b41da00a906ec15dd0f218afdb
bleatingsheep <bleatingsheep@live.cn>
2025-09-13 04:08:37 +0800

    misc: 将每分钟更新的记录数提升到160。

--- Bleatingsheep.NewHydrant.DataMaintenance/UpdateSnapshotsService.cs
+++ Bleatingsheep.NewHydrant.DataMaintenance/UpdateSnapshotsService.cs
@@ -3,6 +3,7 @@ using Bleatingsheep.OsuQqBot.Database.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bleatingsheep.NewHydrant.DataMaintenance;
+
 public class UpdateSnapshotsService : BackgroundService
 {
     private static readonly TimeSpan s_updateScheduleDefault = TimeSpan.FromHours(8);
@@ -15,7 +16,11 @@ public class UpdateSnapshotsService : BackgroundService
     private readonly DataMaintainer _dataMaintainer;
     private readonly ILogger<UpdateSnapshotsService> _logger;
 
-    public UpdateSnapshotsService(IDbContextFactory<NewbieContext> dbContextFactory, DataMaintainer dataMaintainer, ILogger<UpdateSnapshotsService> logger)
+    public UpdateSnapshotsService(
+        IDbContextFactory<NewbieContext> dbContextFactory,
+        DataMaintainer dataMaintainer,
+        ILogger<UpdateSnapshotsService> logger
+    )
     {
         _dbContextFactory = dbContextFactory;
         _dataMaintainer = dataMaintainer;
@@ -25,24 +30,37 @@ public class UpdateSnapshotsService : BackgroundService
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         var timer = new PeriodicTimer(TimeSpan.FromMinutes(1));
-        while (await timer.WaitForNextTickAsync(stoppingToken) && !stoppingToken.IsCancellationRequested)
+        while (
+            await timer.WaitForNextTickAsync(stoppingToken)
+            && !stoppingToken.IsCancellationRequested
+        )
         {
             try
             {
                 await using var db = _dbContextFactory.CreateDbContext();
-                int scheduledCount = await db.UpdateSchedules.CountAsync(s => s.NextUpdate <= DateTimeOffset.UtcNow, stoppingToken);
+                int scheduledCount = await db.UpdateSchedules.CountAsync(
+                    s => s.NextUpdate <= DateTimeOffset.UtcNow,
+                    stoppingToken
+                );
                 if (scheduledCount == 0)
+                {
                     continue;
+                }
+
                 if (scheduledCount >= 500)
                 {
                     _logger.LogWarning("Backlog is {count}", scheduledCount);
                 }
-                var toUpdate = await db.UpdateSchedules
-                    .Where(s => s.NextUpdate <= DateTimeOffset.UtcNow)
+                var toUpdate = await db
+                    .UpdateSchedules.Where(s => s.NextUpdate <= DateTimeOffset.UtcNow)
                     .OrderBy(s => s.NextUpdate)
-                    .Take(120)
+                    .Take(160)
                     .ToListAsync(stoppingToken);
-                _logger.LogDebug("Updating {toUpdate.Count} of {scheduledCount} snapshots.", toUpdate.Count, scheduledCount);
+                _logger.LogDebug(
+                    "Updating {toUpdate.Count} of {scheduledCount} snapshots.",
+                    toUpdate.Count,
+                    scheduledCount
+                );
                 int successCount = 0;
                 int normalCount = 0;
                 int activeCount = 0;
@@ -53,7 +71,10 @@ public class UpdateSnapshotsService : BackgroundService
                 {
                     try
                     {
-                        var report = await _dataMaintainer.UpdateNowAsync(schedule.UserId, schedule.Mode);
+                        var report = await _dataMaintainer.UpdateNowAsync(
+                            schedule.UserId,
+                            schedule.Mode
+                        );
                         TimeSpan nextDelay;
                         if (report.UserNotExists || report.Inactive)
                         {
@@ -87,26 +108,53 @@ public class UpdateSnapshotsService : BackgroundService
                     {
                         if (e.Message.Contains("429 Too Many Requests"))
                         {
-                            _logger.LogInformation("Reached API rate limit on user id {schedule.UserId} mode {schedule.Mode}", schedule.UserId, schedule.Mode);
+                            _logger.LogInformation(
+                                "Reached API rate limit on user id {schedule.UserId} mode {schedule.Mode}",
+                                schedule.UserId,
+                                schedule.Mode
+                            );
                         }
                         else if (e.Message.Contains("502 Bad Gateway"))
                         {
-                            _logger.LogInformation("Server error, breaking to wait for server to get normal. user id {schedule.UserId} mode {schedule.Mode}", schedule.UserId, schedule.Mode);
+                            _logger.LogInformation(
+                                "Server error, breaking to wait for server to get normal. user id {schedule.UserId} mode {schedule.Mode}",
+                                schedule.UserId,
+                                schedule.Mode
+                            );
                         }
                         else
                         {
-                            _logger.LogError(e, "Update error on user id {schedule.UserId} mode {schedule.Mode}", schedule.UserId, schedule.Mode);
+                            _logger.LogError(
+                                e,
+                                "Update error on user id {schedule.UserId} mode {schedule.Mode}",
+                                schedule.UserId,
+                                schedule.Mode
+                            );
                         }
                         break;
                     }
                 }
                 await db.SaveChangesAsync(stoppingToken).ConfigureAwait(false); // 将 schedule 更新持久化到数据库
-                _logger.LogDebug("Update schedule completed. Success {successCount} of {toUpdate.Count}.", successCount, toUpdate.Count);
-                _logger.LogDebug("Normal: {normalCount}, Active: {activeCount}, SemiActive: {semiActiveCount}, Inactive: {inactiveCount}, No Change: {noChangeCount}", normalCount, activeCount, semiActiveCount, inactiveCount, noChangeCount);
+                _logger.LogDebug(
+                    "Update schedule completed. Success {successCount} of {toUpdate.Count}.",
+                    successCount,
+                    toUpdate.Count
+                );
+                _logger.LogDebug(
+                    "Normal: {normalCount}, Active: {activeCount}, SemiActive: {semiActiveCount}, Inactive: {inactiveCount}, No Change: {noChangeCount}",
+                    normalCount,
+                    activeCount,
+                    semiActiveCount,
+                    inactiveCount,
+                    noChangeCount
+                );
             }
             catch (DbUpdateConcurrencyException e)
             {
-                _logger.LogWarning("数据库更新并发冲突，可能是其他服务修改了更新 schedule：{message}。", e.Message);
+                _logger.LogWarning(
+                    "数据库更新并发冲突，可能是其他服务修改了更新 schedule：{message}。",
+                    e.Message
+                );
             }
             catch (Exception e)
             {
